e69a53d8e4f22f8da420caacdbc5ed5a884d72ba
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-04-21 14:14:24 +0200

    Hibernate: Use CriteriaBuilder instead of HQL syntax

--- src/main/java/de/uni_tuebingen/ub/nppm/controller/BenutzerController.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/controller/BenutzerController.java
@@ -2,32 +2,48 @@ package de.uni_tuebingen.ub.nppm.controller;
 
 import java.util.List;
 import org.hibernate.Session;
-import de.uni_tuebingen.ub.nppm.model.Benutzer;
-import de.uni_tuebingen.ub.nppm.model.BenutzerGruppe;
+import de.uni_tuebingen.ub.nppm.model.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class BenutzerController extends AbstractBase {
 
     public static List getList() throws Exception {
         Session session = getSession();
-        List<Benutzer> list = session.createQuery("FROM Benutzer ORDER BY Nachname ASC, Vorname ASC", Benutzer.class).getResultList();
-        return list;
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Benutzer> criteria = builder.createQuery(Benutzer.class);
+        Root benutzer = criteria.from(Benutzer.class);
+        criteria.select(benutzer);
+        criteria.orderBy(builder.asc(benutzer.get(Benutzer_.Nachname)), builder.asc(benutzer.get(Benutzer_.Vorname)));
+
+        return session.createQuery(criteria).getResultList();
     }
 
     public static List getListAktiv() throws Exception {
         Session session = getSession();
-        List<Benutzer> list = session.createQuery("FROM Benutzer WHERE IstAktiv=1 ORDER BY Nachname ASC, Vorname ASC", Benutzer.class).getResultList();
-        return list;
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Benutzer> criteria = builder.createQuery(Benutzer.class);
+        Root benutzer = criteria.from(Benutzer.class);
+        criteria.select(benutzer);
+        criteria.where(builder.isTrue(benutzer.get(Benutzer_.IstAktiv)));
+        criteria.orderBy(builder.asc(benutzer.get(Benutzer_.Nachname)), builder.asc(benutzer.get(Benutzer_.Vorname)));
+
+        return session.createQuery(criteria).getResultList();
     }
 
     public static List getListInaktiv() throws Exception {
         Session session = getSession();
-        List<Benutzer> list = session.createQuery("FROM Benutzer WHERE IstAktiv=0 ORDER BY Nachname ASC, Vorname ASC", Benutzer.class).getResultList();
-        return list;
-    }
 
-    public static String getGruppeBezeichnung(int ID) throws Exception {
-        Session session = getSession();
-        BenutzerGruppe gruppe = session.createQuery("FROM BenutzerGruppe WHERE ID=" + Integer.toString(ID), BenutzerGruppe.class).getSingleResult();
-        return gruppe.getBezeichnung();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Benutzer> criteria = builder.createQuery(Benutzer.class);
+        Root benutzer = criteria.from(Benutzer.class);
+        criteria.select(benutzer);
+        criteria.where(builder.isFalse(benutzer.get(Benutzer_.IstAktiv)));
+        criteria.orderBy(builder.asc(benutzer.get(Benutzer_.Nachname)), builder.asc(benutzer.get(Benutzer_.Vorname)));
+
+        return session.createQuery(criteria).getResultList();
     }
 }
