33f49e0edafddfdee462213a11fbea282e00c24a
jrost <julianrost@posteo.de>
2022-05-10 16:14:39 +0200

    Add JUnit Classes for Controller

---
+++ src/test/java/de/uni_tuebingen/ub/nppm/controller/test/EditionControllerTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.uni_tuebingen.ub.nppm.controller.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import de.uni_tuebingen.ub.nppm.controller.EditionController;
+
+/**
+ *
+ * @author julian
+ */
+public class EditionControllerTest {
+    
+    @Test                                               
+    @DisplayName("List Edition Entities")   
+    void testList() {
+        try{
+            assertFalse(EditionController.getList().isEmpty(),"List is empty");  
+        }catch(Exception e){
+            fail(e.getLocalizedMessage());
+        }
+    }
+}
