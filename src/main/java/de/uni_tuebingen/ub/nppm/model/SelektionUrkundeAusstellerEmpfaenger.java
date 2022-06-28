755c417a061745f98e91cbbf18498e0efe42c43d
jrost <julian.rost@uni-tuebingen.de>
2022-06-28 16:22:28 +0200

    Add Urkunde Entity

--- src/main/java/de/uni_tuebingen/ub/nppm/model/SelektionUrkundeAusstellerEmpfaenger.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/model/SelektionUrkundeAusstellerEmpfaenger.java
@@ -13,6 +13,50 @@ public class SelektionUrkundeAusstellerEmpfaenger {
     @Column(name = "Bezeichnung", length=255)
     private String bezeichnung;
 
+    @ManyToMany(mappedBy = "empfaenger")
+    private List<Urkunde> urkundeEmpfaenger = new ArrayList<Urkunde>();
+    
+    @ManyToMany(mappedBy = "aussteller")
+    private List<Urkunde> urkundeAussteller = new ArrayList<Urkunde>();;
+    
+    public List<Urkunde> getUrkundeEmpfaenger() {
+        return this.urkundeEmpfaenger;
+    }
+    
+    public void addUrkundeEmpfaenger(Urkunde uk){
+        this.getUrkundeEmpfaenger().add(uk);
+    }
+      
+    public void removeUrkundeEmpfaenger(int id){
+        for (int i = 0; i < this.getUrkundeEmpfaenger().size(); ) {
+            Urkunde uk = this.getUrkundeEmpfaenger().get(i);
+            if(uk.getId() != null && uk.getId() == id){
+                this.getUrkundeEmpfaenger().remove(i);
+            }else{
+                i++;
+            }
+        }
+    }
+    
+    public List<Urkunde> getUrkundeAussteller() {
+        return this.urkundeAussteller;
+    }
+    
+    public void addUrkundeAussteller(Urkunde uk){
+        this.getUrkundeAussteller().add(uk);
+    }
+      
+    public void removeUrkundeAussteller(int id){
+        for (int i = 0; i < this.getUrkundeAussteller().size(); ) {
+            Urkunde uk = this.getUrkundeAussteller().get(i);
+            if(uk.getId() != null && uk.getId() == id){
+                this.getUrkundeAussteller().remove(i);
+            }else{
+                i++;
+            }
+        }
+    }
+    
     public Integer getId() {
         return id;
     }
---
+++ src/main/java/de/uni_tuebingen/ub/nppm/model/Urkunde.java
@@ -0,0 +1,93 @@
+package de.uni_tuebingen.ub.nppm.model;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "urkunde")
+public class Urkunde {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+    
+    @OneToOne(targetEntity = Quelle.class)
+    @JoinColumn(name = "QuelleID", referencedColumnName="ID")
+    private Quelle quelle;
+
+    @Column(name = "Actumort", length = 255)
+    private String actumort;
+    
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+        name = "urkunde_hataussteller", 
+        joinColumns = { @JoinColumn(name = "AusstellerID") }, 
+        inverseJoinColumns = { @JoinColumn(name = "UrkundeID") }
+    )
+    private List<SelektionUrkundeAusstellerEmpfaenger> aussteller = new ArrayList<SelektionUrkundeAusstellerEmpfaenger>();;
+    
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+        name = "urkunde_hatempfaenger", 
+        joinColumns = { @JoinColumn(name = "EmpfaengerID") }, 
+        inverseJoinColumns = { @JoinColumn(name = "UrkundeID") }
+    )
+    private List<SelektionUrkundeAusstellerEmpfaenger> empfaenger = new ArrayList<SelektionUrkundeAusstellerEmpfaenger>();
+    
+    public List<SelektionUrkundeAusstellerEmpfaenger> getEmpfaenger() {
+        return this.empfaenger;
+    }
+    
+    public void addEmpfaenger(SelektionUrkundeAusstellerEmpfaenger sel){
+        this.getEmpfaenger().add(sel);
+    }
+      
+    public void removeEmpfaenger(int id){
+        for (int i = 0; i < this.getEmpfaenger().size(); ) {
+            SelektionUrkundeAusstellerEmpfaenger sel = this.getEmpfaenger().get(i);
+            if(sel.getId() != null && sel.getId() == id){
+                this.getEmpfaenger().remove(i);
+            }else{
+                i++;
+            }
+        }
+    }
+    
+    public List<SelektionUrkundeAusstellerEmpfaenger> getAussteller() {
+        return this.aussteller;
+    }
+    
+    public void addAussteller(SelektionUrkundeAusstellerEmpfaenger sel){
+        this.getAussteller().add(sel);
+    }
+      
+    public void removeAussteller(int id){
+        for (int i = 0; i < this.getAussteller().size(); ) {
+            SelektionUrkundeAusstellerEmpfaenger sel = this.getAussteller().get(i);
+            if(sel.getId() != null && sel.getId() == id){
+                this.getAussteller().remove(i);
+            }else{
+                i++;
+            }
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Quelle getQuelle() {
+        return quelle;
+    }
+
+    public void setQuelle(Quelle quelle) {
+        this.quelle = quelle;
+    }
+
+    public String getActumort() {
+        return actumort;
+    }
+
+    public void setActumort(String actumort) {
+        this.actumort = actumort;
+    }
+}
