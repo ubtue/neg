43b01cd6b08302f61e8f32f619a52d8be01d5649
jrost <julian.rost@uni-tuebingen.de>
2022-07-13 09:23:39 +0200

    Catch Exception in AbstractBase

--- src/main/java/de/uni_tuebingen/ub/nppm/db/AbstractBase.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/AbstractBase.java
@@ -10,10 +10,10 @@ import org.hibernate.service.ServiceRegistry;
 
 import de.uni_tuebingen.ub.nppm.model.*;
 import java.util.List;
+import javax.naming.NamingException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.persistence.metamodel.SingularAttribute;
 
 public class AbstractBase {
     protected static SessionFactory sessionFactory;
@@ -25,7 +25,7 @@ public class AbstractBase {
     }
     
     // Example taken from: https://www.javaguides.net/2019/08/hibernate-5-one-to-many-mapping-annotation-example.html
-    protected static SessionFactory getSessionFactory() throws Exception {
+    protected static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             // Hibernate settings equivalent to hibernate.cfg.xml's properties
             Configuration configuration = new Configuration();
@@ -34,13 +34,18 @@ public class AbstractBase {
             Properties settings = new Properties();
 
             // Get settings from tomcat config
-            if(initialContext == null)
-                initialContext = new javax.naming.InitialContext();
-            settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-            settings.put(Environment.URL, (String)initialContext.lookup("java:comp/env/sqlURL"));
-            settings.put(Environment.USER, (String)initialContext.lookup("java:comp/env/sqlUser"));
-            settings.put(Environment.PASS, (String)initialContext.lookup("java:comp/env/sqlPassword"));
-            
+            try {
+                if (initialContext == null) {
+                    initialContext = new javax.naming.InitialContext();
+                }
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.URL, (String)initialContext.lookup("java:comp/env/sqlURL"));
+                settings.put(Environment.USER, (String)initialContext.lookup("java:comp/env/sqlUser"));
+                settings.put(Environment.PASS, (String)initialContext.lookup("java:comp/env/sqlPassword"));
+            } catch (NamingException namingException) {
+                namingException.printStackTrace();
+                return null;
+            }
             // This must be changed when migrating to InnoDB
             //settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5InnoDBDialect");
             settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLMyISAMDialect");
@@ -155,11 +160,11 @@ public class AbstractBase {
 
     // For now, we open a new session each time this method is called.
     // Later, we might try to use a static session similar to the static SessionFactory.
-    protected static Session getSession() throws Exception {
+    protected static Session getSession() {
         return getSessionFactory().openSession();
     }
 
-    protected static List getList(Class c, CriteriaQuery criteria) throws Exception {
+    protected static List getList(Class c, CriteriaQuery criteria) {
         Session session = getSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
         if(criteria == null){
@@ -170,7 +175,7 @@ public class AbstractBase {
         return session.createQuery(criteria).getResultList();
     }
 
-    protected static List getList(Class c) throws Exception {
+    protected static List getList(Class c) {
         return getList(c, null);
     }
 }
