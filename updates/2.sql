4205bc1e2352904b999113b967c047f24739c902
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-07-22 11:44:00 +0200

    Add update to remove literatur, see #27

---
+++ updates/2.sql
@@ -0,0 +1,9 @@
+#Literatur 
+DROP TABLE literatur;
+DROP TABLE literatur_autor;
+DROP TABLE literatur_herausgeber;
+DROP TABLE literatur_sw_arealgens;
+DROP TABLE literatur_sw_morphologie;
+DROP TABLE literatur_sw_namenelemente;
+DROP TABLE literatur_sw_phonograph;
+
