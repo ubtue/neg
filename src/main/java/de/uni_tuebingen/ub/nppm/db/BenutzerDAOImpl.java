c3b9ced4635fd218c87d0b4f03257e3e97b0c5ed
jrost <julianrost@posteo.de>
2022-08-11 10:42:17 +0200

    Add Missing Annotation

--- src/main/java/de/uni_tuebingen/ub/nppm/db/BenutzerDAOImpl.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/BenutzerDAOImpl.java
@@ -6,7 +6,11 @@ import de.uni_tuebingen.ub.nppm.model.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Component
 public class BenutzerDAOImpl extends AbstractBase implements BenutzerDAO {
 
     @Override
