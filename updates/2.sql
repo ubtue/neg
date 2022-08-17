e4b27d2c374e78a3066305433c41fe5a1ba9992b
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-08-17 08:47:34 +0200

    SQL updates: Change DROP TABLE to DROP TABLE IF EXISTS (idempotency)

--- updates/2.sql
+++ updates/2.sql
@@ -1,9 +1,9 @@
-#Literatur 
+#Literatur
 ALTER TABLE bemerkung DROP column LiteraturID;
-DROP TABLE literatur;
-DROP TABLE literatur_autor;
-DROP TABLE literatur_herausgeber;
-DROP TABLE literatur_sw_arealgens;
-DROP TABLE literatur_sw_morphologie;
-DROP TABLE literatur_sw_namenelemente;
-DROP TABLE literatur_sw_phongraph;
+DROP TABLE IF EXISTS literatur;
+DROP TABLE IF EXISTS literatur_autor;
+DROP TABLE IF EXISTS literatur_herausgeber;
+DROP TABLE IF EXISTS literatur_sw_arealgens;
+DROP TABLE IF EXISTS literatur_sw_morphologie;
+DROP TABLE IF EXISTS literatur_sw_namenelemente;
+DROP TABLE IF EXISTS literatur_sw_phongraph;
--- updates/5.sql
+++ updates/5.sql
@@ -2,4 +2,4 @@
 DELETE FROM datenbank_selektion WHERE selektion = "selektion_literaturtyp";
 
 #drop table selektion_literaturtyp
-DROP TABLE selektion_literaturtyp;
+DROP TABLE IF EXISTS selektion_literaturtyp;
