23f3077b9cfcc8d04369178760266efc19a98445
jrost <julianrost@posteo.de>
2022-08-23 10:13:09 +0200

    Create Person Gast Controller

--- src/main/java/de/uni_tuebingen/ub/nppm/controller/GastPersonController.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/controller/GastPersonController.java
@@ -15,36 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import de.uni_tuebingen.ub.nppm.model.*;
 
 @Controller
-@RequestMapping("/person")
-public class PersonController {
-
+@RequestMapping("/gast/person")
+public class GastPersonController {
     @Autowired
     private PersonService personService;
 
     @GetMapping("/showForm")
-    public String showFormForAdd(Model model) {
-        Person person = new Person();
-        model.addAttribute("person", person);
-        return "person/person-form";
-    }
-
-    @PostMapping("/savePerson")
-    public String addPerson(@ModelAttribute("person") Person person) {
-        personService.addPerson(person);
-        return "redirect:/person/showForm";
-    }
-
-    @GetMapping("/updateForm")
-    public String showFormForUpdate(@RequestParam("personId") int id,
-            Model model) {
+    public String showForm(@RequestParam("personId") int id, Model model) {
         Person person = personService.getPersonById(id);
         model.addAttribute("person", person);
-        return "person/person-form";
-    }
-
-    @GetMapping("/remove")
-    public String removePerson(@RequestParam("personId") int id) {
-        personService.removePerson(id);
-        return "redirect:/person/showForm";
+        return "person/gast/person-form";
     }
 }
