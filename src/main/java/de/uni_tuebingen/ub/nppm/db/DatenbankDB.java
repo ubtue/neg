0802e12a529e8d1557e8635a66329ac6beba45c0
jrost <julianrost@posteo.de>
2022-09-06 10:58:01 +0200

    Add Database Function for Filters

--- src/main/java/de/uni_tuebingen/ub/nppm/db/DatenbankDB.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/DatenbankDB.java
@@ -2,6 +2,9 @@ package de.uni_tuebingen.ub.nppm.db;
 
 import java.util.List;
 import de.uni_tuebingen.ub.nppm.model.*;
+import org.hibernate.*;
+import javax.persistence.criteria.*;
+import javax.servlet.jsp.JspWriter;
 
 public class DatenbankDB extends AbstractBase {
 
@@ -24,4 +27,29 @@ public class DatenbankDB extends AbstractBase {
     public static List getListTexte() throws Exception {
         return getList(DatenbankTexte.class);
     }
+    
+    public static String getFilterSql(String formular, Integer filterNumber){
+        try {
+            Session session = getSession();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<DatenbankFilter> criteria = criteriaBuilder.createQuery(DatenbankFilter.class);
+            Root<DatenbankFilter> root = criteria.from(DatenbankFilter.class);
+            criteria.select(root).where(
+                    criteriaBuilder.and(
+                        criteriaBuilder.equal(root.get("nummer"), filterNumber),
+                        criteriaBuilder.equal(root.get("formular"), formular)    
+                    )
+            );
+            Query query = session.createQuery(criteria);
+            DatenbankFilter item = (DatenbankFilter)query.getSingleResult();            
+            if(item != null){
+                return item.getSqlString();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+    
+    
 }
