bfa11e3d4b63f2c02e6775bb4824cf2366e69aa4
jrost <julianrost@posteo.de>
2022-09-08 08:32:02 +0200

    Extend Language Helper

--- src/main/java/de/uni_tuebingen/ub/nppm/util/Language.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/util/Language.java
@@ -2,7 +2,8 @@ package de.uni_tuebingen.ub.nppm.util;
 
 import java.io.*;
 import javax.servlet.http.*;
-
+import de.uni_tuebingen.ub.nppm.db.DatenbankDB;
+import javax.servlet.jsp.JspWriter;
 public class Language {
 
     public static void setLanguage(HttpServletRequest request) {
@@ -26,5 +27,26 @@ public class Language {
 
             }
         }
+    }
+    
+    public static void printDatafield(JspWriter out,HttpSession session, String formular, String datenfeld) throws IOException{
+        out.print("<label for=\""+datenfeld+"\">");
+        printLabel(out,session,formular,datenfeld,null);  
+        out.print("</label>");
+    }
+    public static void printTextfield(JspWriter out,HttpSession session, String formular, String textfield) throws IOException{
+        printLabel(out,session,formular,null,textfield);   
+    }
+    
+    private static void printLabel(JspWriter out,HttpSession session, String formular, String datenfeld, String textfeld) throws IOException{
+        String lang = "de";
+        //try to get language from session
+        if (session != null && session.getAttribute("Sprache") != null)
+            lang = (String)session.getAttribute("Sprache");
+        if (datenfeld == null && textfeld != null) {
+            out.println(DatenbankDB.getLabel(lang, formular, textfeld));
+        }else if (datenfeld != null && textfeld == null) {
+            out.println(DatenbankDB.getMapping(lang, formular, datenfeld));
+        }        
     }
 }
