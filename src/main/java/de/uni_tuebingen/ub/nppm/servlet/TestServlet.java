25f488b2946cafd376a44a47ca39ac5cdc9727a8
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-09-23 09:41:08 +0200

    TestServlet

---
+++ src/main/java/de/uni_tuebingen/ub/nppm/servlet/TestServlet.java
@@ -0,0 +1,25 @@
+package de.uni_tuebingen.ub.nppm.servlet;
+
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        request.setAttribute("text", "TestServlet says hello!");
+
+        RequestDispatcher rd = request.getRequestDispatcher("test/test.jsp");
+        rd.forward(request, response);
+    }
+}
