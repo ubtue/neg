3487532e69dc4127d31f8a7200ba84c56c68e4cc
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-09-27 10:16:44 +0200

    NewPasswordServlet: Also check min pw length here

--- src/main/java/de/uni_tuebingen/ub/nppm/servlet/NewPasswordServlet.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/servlet/NewPasswordServlet.java
@@ -54,7 +54,7 @@ public class NewPasswordServlet extends HttpServlet {
             writeHTMLMessage(request, response, message);
         } else {
             Benutzer benutzer = BenutzerDB.getByMail(URLemail);
-            if (password != null && !password.equals("") && repeatPassword.equals(password)) {
+            if (password != null && password.length() >= 6 && repeatPassword.equals(password)) {
                 LocalDateTime pastDateTime = benutzer.getResetTokenValidUntil();
                 LocalDateTime nowDateTime = LocalDateTime.now();
                 long timeBetween = ChronoUnit.HOURS.between(pastDateTime, nowDateTime);
@@ -81,6 +81,10 @@ public class NewPasswordServlet extends HttpServlet {
                     message[1] = "<h1 style=\"text-align: center;\"><a href=\"" + Utils.getBaseUrl(request) + "/forgotPassword\">Neuen Link generieren</a></h1>";
                     writeHTMLMessage(request, response, message);
                 }
+            } else if(password != null && password.length() < 6) {
+                String[] message = new String[1];
+                message[0] = "<h1 style=\"text-align: center;\">Passwort ist zu kurz, mindestlänge sind 6 Buchstaben</h1>";
+                writeHTMLMessage(request, response, message);
 
             } else if (!repeatPassword.equals(password)) {
                 String[] message = new String[1];
