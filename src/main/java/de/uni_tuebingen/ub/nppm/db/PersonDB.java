e8fc8e3ad11f6b8806ab333ad3903ee47dc78889
rnaiser <ryan.naiser@uni-tuebingen.de>
2022-09-29 08:58:39 +0200

    Miraculix Hütte

--- src/main/java/de/uni_tuebingen/ub/nppm/db/PersonDB.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/PersonDB.java
@@ -1,23 +1,35 @@
 package de.uni_tuebingen.ub.nppm.db;
 
+import static de.uni_tuebingen.ub.nppm.db.AbstractBase.getSession;
 import java.util.List;
 import de.uni_tuebingen.ub.nppm.model.*;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 public class PersonDB extends AbstractBase {
 
     public static List getListPerson() throws Exception {
         return getList(Person.class);
     }
-    
+
     public static List getListPersonAmtStandWeihe() throws Exception {
         return getList(PersonAmtStandWeihe_MM.class);
     }
-    
+
     public static List getListPersonQuiet() throws Exception {
         return getList(PersonQuiet.class);
     }
-    
+
     public static List getListPersonVariante() throws Exception {
         return getList(PersonVariante.class);
+    }
+
+    public static Einzelbeleg getFirstPublicPerson() throws Exception {
+        Session session = getSession();
+        //String HQL = "FROM Einzelbeleg WHERE QuelleID IN (SELECT id FROM Quelle WHERE ZuVeroeffentlichen=1) ORDER BY id ASC";
+        String HQL = "FROM Person WHERE id IN (SELECT PersonID FROM einzelbeleg_hatperson WHERE EinzelbelegID IN (SELECT einzelbeleg.ID FROM einzelbeleg, quelle WHERE einzelbeleg.QuelleID=quelle.ID AND quelle.ZuVeroeffentlichen=1)) ORDER BY id ASC";
+        Query query = session.createQuery(HQL).setMaxResults(1);
+        Einzelbeleg einzelbeleg = (Einzelbeleg)query.getSingleResult();
+        return einzelbeleg;
     }
 }
---
+++ src/main/java/de/uni_tuebingen/ub/nppm/servlet/gast/PersonServlet.java
@@ -0,0 +1,28 @@
+package de.uni_tuebingen.ub.nppm.servlet.gast;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import de.uni_tuebingen.ub.nppm.db.PersonDB;
+
+public class PersonServlet extends AbstractGastServlet {
+    @Override
+    protected String getTitle() {
+        return "person";
+    }
+
+    @Override
+    protected String getNavigationTitle() {
+        return "person";
+    }
+
+    @Override
+    protected void generatePage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (request.getParameter("ID") == null) {
+            response.sendRedirect(request.getContextPath() + "/gast/person?ID=" + PersonDB.getFirstPublicPerson().getId());
+        } else {
+            RequestDispatcher rd = request.getRequestDispatcher("person.jsp");
+            rd.include(request, response);
+        }
+    }
+}
