24252cd71eac33d21fcf61f96d69f8e4c16ace9c
jrost <julianrost@posteo.de>
2022-11-09 11:26:53 +0100

    Refactor Ergebnisliste

--- src/main/java/de/uni_tuebingen/ub/nppm/db/SucheDB.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/SucheDB.java
@@ -3,6 +3,8 @@ package de.uni_tuebingen.ub.nppm.db;
 import static de.uni_tuebingen.ub.nppm.db.AbstractBase.getSession;
 import java.util.List;
 import de.uni_tuebingen.ub.nppm.model.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
@@ -47,6 +49,38 @@ public class SucheDB extends AbstractBase {
                 ret.put(Integer.valueOf(row[0].toString()), row[1].toString());
             }
         }     
+        
+        return ret;
+    }
+    
+    public static int getLinecount(String tablesString, String conditionsString) throws Exception {
+        String sql = "SELECT COUNT(*) FROM "+tablesString+" WHERE ("+conditionsString+")";
+        Session session = getSession();
+        SQLQuery sqlQuery = session.createSQLQuery(sql);
+        sqlQuery.setMaxResults(1);
+        List<Object> rows = sqlQuery.getResultList();
+        return (int)rows.get(0);
+    }
+        
+    public static List<Map<String,String>> getFields(String fields , String tablesString, String conditionsString, String export, int pageoffset, int pageLimit) throws Exception {
+        String sql = "SELECT "+fields+" FROM "+tablesString+" WHERE ("+conditionsString+")";
+        if (export.equals("liste") || export.equals("browse"))
+            sql += " LIMIT "+(pageoffset*pageLimit)+", "+pageLimit;
+        Session session = getSession();
+        SQLQuery sqlQuery = session.createSQLQuery(sql);
+        List<Object[]> rows = sqlQuery.getResultList();
+        //convert field str to list
+        List<String> fieldList = new ArrayList<String>(Arrays.asList(fields.split(",")));
+        List<Map<String,String>> ret = new ArrayList<Map<String,String>>();
+        //loop over the rows
+        for(Object[] row : rows){         
+            //convert the fields from the row to a map
+            Map<String, String> fieldVal = new HashMap<String, String>();
+            for(int i = 0; i < fieldList.size(); i++){
+                fieldVal.put(fieldList.get(i), row[i].toString());
+            }
+            ret.add(fieldVal);
+        }
         
         return ret;
     }
