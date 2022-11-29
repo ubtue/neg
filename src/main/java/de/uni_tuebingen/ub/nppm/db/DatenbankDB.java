46aeef7ad27a7efa226afe95c4aabca36a48a1e2
jrost <julianrost@posteo.de>
2022-11-29 16:30:36 +0100

    Create DB Functions

--- src/main/java/de/uni_tuebingen/ub/nppm/db/DatenbankDB.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/DatenbankDB.java
@@ -1,10 +1,12 @@
 package de.uni_tuebingen.ub.nppm.db;
 
+import static de.uni_tuebingen.ub.nppm.db.AbstractBase.getSession;
 import java.util.List;
 import de.uni_tuebingen.ub.nppm.model.*;
 import javax.persistence.TypedQuery;
 import org.hibernate.*;
 import javax.persistence.criteria.*;
+import org.hibernate.query.NativeQuery;
 
 public class DatenbankDB extends AbstractBase {
 
@@ -117,4 +119,47 @@ public class DatenbankDB extends AbstractBase {
         }
     }
 
+    public static int getMaxId(String form) throws Exception {
+        return (int)DatenbankDB.getSingleResult("SELECT max(ID) ID FROM "+form);
+    }
+    
+    public static boolean existForm(String form, int id) throws Exception {
+        return DatenbankDB.getSingleResult("SELECT * FROM "+form+" WHERE ID="+id) != null;
+    }
+    
+    public static void insertMetaData(String form, int id, int benutzerID, int gruppenID) throws Exception {
+        String sql = "INSERT INTO "+form+" (ID, Erstellt, ErstelltVon, GehoertGruppe)"
+                    +" VALUES("+id+", NOW(), "+benutzerID+", "+gruppenID+");";
+        //insert
+        Session session = getSession();
+        session.getTransaction().begin();
+        NativeQuery query = session.createSQLQuery(sql);
+        query.executeUpdate();
+        session.getTransaction().commit();      
+    }
+    
+    public static void updateMetaData(String form, int id, int benutzerID) throws Exception {
+        String sql = "UPDATE "+form+" SET LetzteAenderung = NOW(), LetzteAenderungVon=\""+benutzerID+"\" WHERE ID="+id+";";
+        //update
+        Session session = getSession();
+        session.getTransaction().begin();
+        NativeQuery query = session.createSQLQuery(sql);
+        query.executeUpdate();
+        session.getTransaction().commit();      
+    }
+    
+    public static List<DatenbankMapping> getMapping(String formular) throws Exception {
+        Session session = getSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<DatenbankMapping> criteria = criteriaBuilder.createQuery(DatenbankMapping.class);
+        Root<DatenbankMapping> root = criteria.from(DatenbankMapping.class);
+        criteria.select(root).where(
+                criteriaBuilder.and(
+                        criteriaBuilder.equal(root.get("formular"), formular)
+                )
+        );
+        org.hibernate.query.Query query = session.createQuery(criteria);
+        List<DatenbankMapping> rows = query.getResultList();
+        return rows;
+    }
 }
