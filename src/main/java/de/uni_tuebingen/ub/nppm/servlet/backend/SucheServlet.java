36f87ac3b5323def57a503cd75b74a8080d70947
jrost <julianrost@posteo.de>
2022-12-15 10:50:07 +0100

    Modify Header Includes

--- src/main/java/de/uni_tuebingen/ub/nppm/servlet/backend/SucheServlet.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/servlet/backend/SucheServlet.java
@@ -1,5 +1,7 @@
 package de.uni_tuebingen.ub.nppm.servlet.backend;
 
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,5 +16,15 @@ public class SucheServlet extends AbstractBackendServlet {
     protected void generatePage(HttpServletRequest request, HttpServletResponse response) throws Exception {
         RequestDispatcher rd = request.getRequestDispatcher("suche.jsp");
         rd.include(request, response);
+    }
+    
+    @Override
+    protected List<String> getAdditionalCss() {
+        return Arrays.asList("mktree.css");
+    }
+
+    @Override
+    protected List<String> getAdditionalJavaScript() {
+        return Arrays.asList("mktree.js");
     }
 }
--- src/main/webapp/servlet/header.jsp
+++ src/main/webapp/servlet/header.jsp
@@ -2,11 +2,8 @@
   <HEAD>
     <TITLE>Nomen et Gens - ${title}</TITLE>
     <link rel="stylesheet" href="layout/layout.css" type="text/css">
-
-    <link rel="stylesheet" href="mktree.css" type="text/css">
     
     <script src="javascript/funktionen.js" type="text/javascript"></script>
-    <script type="text/javascript" src="mktree.js"></script>
 
     <link rel="stylesheet" href="layout/jquery-ui-1.10.3.css" type="text/css">
     <link rel="stylesheet" href="layout/jquery.autocomplete.css" type="text/css">
