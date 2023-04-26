5336da6c19fe6ad6e88b0293bd9cf3190cc9b197
jrost <julianrost@posteo.de>
2023-04-26 08:30:10 +0200

    Remove Person CM Reference

--- updates/12.sql
+++ updates/12.sql
@@ -1,3 +1,5 @@
 #Clean up CM References
 DELETE from datenbank_mapping where Formular = "quelle" and Datenfeld = "CMRef";
 DELETE from datenbank_mapping where Formular = "quelle" and Datenfeld = "CMLink";
+DELETE from datenbank_mapping where Formular = "person" and Datenfeld = "CMRef";
+DELETE from datenbank_mapping where Formular = "person" and Datenfeld = "CMLink";
