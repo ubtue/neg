6d15dc2747521160b86e1cd5197c67e332655696
jrost <julianrost@posteo.de>
2023-04-27 16:33:32 +0200

    Konvent SQL

---
+++ updates/12.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS `neg`.`selektion_konvent` (
+  `ID` INT NOT NULL,
+  `Bezeichnung` VARCHAR(50) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Kreuzzeichen');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Zugehörigkeit zu "C" und "LE"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Zugehörigkeit zu "CL" (Cluny)');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "C"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: C');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Zugehörigkeit zu "C" (?)');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "C" und "P"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "M" rot durchstrichen');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: F');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('übergeschriebenes "P" und "S"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: G');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "C" und "S"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: H');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Zugehörigkeit zu "LE" (Limoges)');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Zugehörigkeit zu "L" (?)');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "M"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: M');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "M" getilgt');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: N');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: Zugehörigkeit zu "?" (?)');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "P"');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('unbekannter Bezeichner: P');
+INSERT INTO `neg`.`selektion_konvent` (Bezeichnung) VALUES ('Zusatz: übergeschriebenes "S"');
