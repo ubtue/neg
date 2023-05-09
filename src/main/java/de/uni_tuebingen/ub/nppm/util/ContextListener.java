c5db19887a70fa127cf003a46a3793d149309d5b
jrost <julianrost@posteo.de>
2023-05-09 15:33:11 +0200

    Create Servlet Listener

---
+++ src/main/java/de/uni_tuebingen/ub/nppm/util/ContextListener.java
@@ -0,0 +1,56 @@
+package de.uni_tuebingen.ub.nppm.util;
+
+import com.mchange.v2.c3p0.C3P0Registry;
+import com.mchange.v2.c3p0.PooledDataSource;
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Iterator;
+import java.util.Set;
+import javax.servlet.*;
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        C3P0Registry.getNumPooledDataSources();
+
+        /*
+            Close C3P0 Connection Pool
+        */
+        Iterator<Set> it = C3P0Registry.getPooledDataSources().iterator();
+        while (it.hasNext()) {
+            try {
+                   PooledDataSource dataSource = (PooledDataSource) it.next();
+                   dataSource.close();
+            } catch (Exception e) {
+                   e.printStackTrace();
+            }
+        }
+        
+        /*
+            Shutdown AbandonedConnectionCleanupThread
+        */
+        try {
+            AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        /*
+            Deregister drivers
+        */
+        try {
+            while(DriverManager.getDrivers().hasMoreElements()){
+                Driver d = DriverManager.getDrivers().nextElement();
+                DriverManager.deregisterDriver(d);
+            }            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        
+    }
+}
