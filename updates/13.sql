2426c86515b65c83d629f46b9303fdf69f534e16
jrost <julianrost@posteo.de>
2023-06-27 13:52:01 +0200

    Fix SQL Auto Increment

--- updates/13.sql
+++ updates/13.sql
@@ -1,4 +1,5 @@
+--this is necessary because the id column contains 0 values. Otherwise an error appears
 SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
 ALTER TABLE einzelbeleg DROP FOREIGN KEY einzelbeleg_QuelleGattungID;
-ALTER TABLE selektion_quellengattung MODIFY COLUMN ID INT NOT NULL auto_increment;
 ALTER TABLE einzelbeleg ADD CONSTRAINT einzelbeleg_QuelleGattungID FOREIGN KEY (QuelleGattungID) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
+ALTER TABLE selektion_quellengattung MODIFY COLUMN ID INT NOT NULL auto_increment;
