4af9ff997f7a48518801dffc83754fef47d85ea0
rnaiser <ryan.naiser@uni-tuebingen.de>
2023-06-28 09:13:05 +0200

    Password bug fix - no email address entered, field empty

--- src/main/java/de/uni_tuebingen/ub/nppm/servlet/NewPasswordServlet.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/servlet/NewPasswordServlet.java
@@ -90,7 +90,13 @@ public class NewPasswordServlet extends HttpServlet {
         //Check if E-Mail is reguistered in database
         boolean emailIsRegistered = BenutzerDB.hasEmail(email);
 
-        if (email != null && !email.equals("") && emailIsRegistered == true) {
+        if(email.equals(""))
+        {
+            String[] message = new String[1];
+            message[0] = "<h1 style=\"text-align: center;\">Sie haben keine E-mail eingegeben</h1>";
+            writeHTMLMessage(request, response, message);
+        }
+        else if (email != null && !email.equals("") && emailIsRegistered == true) {
 
             response.setContentType("text/html");
 
