cc2a5f7a561d8b8f82bb625155a0b830d08c4c1b
rnaiser <105979344+rnaiser@users.noreply.github.com>
2023-06-28 09:53:44 +0200

    New entry in table datenbank_texte
    to get Inhalt bearbeiten which was prior Hilfe bearbeiten

---
+++ updates/13.sql
@@ -0,0 +1,1 @@
+ INSERT INTO datenbank_texte (Formular, Textfeld, de, gb, fr, la) VALUES ('navigation', 'InhaltBearbeiten', 'Inhalt bearbeiten', 'edit content', 'modifier le contenu', 'recensere content');
