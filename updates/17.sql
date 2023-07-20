3bc3fa719f6dd24ec6994c93cad5dfb2e0c15388
jrost <julianrost@posteo.de>
2023-07-20 10:40:46 +0200

    Move Auto Increment

--- updates/17.sql
+++ updates/17.sql
@@ -1,7 +1,7 @@
 ALTER TABLE einzelbeleg DROP FOREIGN KEY einzelbeleg_QuelleGattungID;
 -- this is necessary because the id column contains 0 values. Otherwise an error appears
-SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
 ALTER TABLE einzelbeleg ADD CONSTRAINT einzelbeleg_QuelleGattungID FOREIGN KEY (QuelleGattungID) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
+SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
 ALTER TABLE selektion_quellengattung MODIFY COLUMN ID INT NOT NULL AUTO_INCREMENT;
 -- restore sql mode
 SET SESSION sql_mode=default;
