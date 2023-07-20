ce51a856b3027b72840c086dbef3faba4427f6d3
jrost <julianrost@posteo.de>
2023-07-20 10:39:01 +0200

    Sql Keyword in Uppercase

--- updates/17.sql
+++ updates/17.sql
@@ -2,6 +2,6 @@ ALTER TABLE einzelbeleg DROP FOREIGN KEY einzelbeleg_QuelleGattungID;
 -- this is necessary because the id column contains 0 values. Otherwise an error appears
 SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
 ALTER TABLE einzelbeleg ADD CONSTRAINT einzelbeleg_QuelleGattungID FOREIGN KEY (QuelleGattungID) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
-ALTER TABLE selektion_quellengattung MODIFY COLUMN ID INT NOT NULL auto_increment;
+ALTER TABLE selektion_quellengattung MODIFY COLUMN ID INT NOT NULL AUTO_INCREMENT;
 -- restore sql mode
 SET SESSION sql_mode=default;
