bfcaf5a221c85060b2fe10ac11594426e87d7714
jrost <julianrost@posteo.de>
2023-07-25 11:11:51 +0200

    Add Kontext Selektion SQL

---
+++ updates/17.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `neg`.`selektion_kontext` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Bezeichnung` VARCHAR(50) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE (`Bezeichnung`)
+  )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+/*Insert a default value*/
+INSERT INTO `neg`.`selektion_kontext` (ID,Bezeichnung) VALUES (-1,'-');
+
+/*Create the datamapping for the kontext attribute of the einzelbeleg form*/
+INSERT INTO datenbank_mapping (Formular, Datenfeld, de_Beschriftung, Feldtyp, Array, ZielTabelle, ZielAttribut, Auswahlherkunft, Seite, gb_beschriftung, fr_beschriftung, la_beschriftung)
+VALUES ("einzelbeleg", "KontextSelektion", "TODO", "select", 0, "einzelbeleg", "KontextID", "selektion_kontext", "einzelbeleg", "TODO", "TODO", "TODO");
+
+ALTER TABLE einzelbeleg ADD KontextID INT DEFAULT -1;
+
+/*Rename existing kontext Beschriftung*/
+UPDATE datenbank_mapping SET de_Beschriftung = 'TODO', gb_beschriftung = 'TODO', fr_beschriftung = 'TODO', la_beschriftung = 'TODO' WHERE Datenfeld = 'Kontext' AND Feldtyp = 'textarea' AND Formular = 'einzelbeleg';
