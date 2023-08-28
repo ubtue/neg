832418f6d41014cc87b87958a5ccff4785855f81
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-08-28 16:11:50 +0200

    SelektionHierarchy: Solve recursion problem in JSON generation

--- src/main/java/de/uni_tuebingen/ub/nppm/model/SelektionHierarchy.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/model/SelektionHierarchy.java
@@ -1,32 +1,39 @@
 package de.uni_tuebingen.ub.nppm.model;
 
 import java.util.Set;
+import javax.persistence.*;
 import org.json.*;
 
+@MappedSuperclass
 abstract public class SelektionHierarchy extends SelektionBezeichnung {
     abstract public SelektionHierarchy getParent();
     abstract public Set<? extends SelektionHierarchy> getChildren();
 
-    public JSONObject toJSONObject() {
+    public JSONObject toJSONObject(boolean ignore_parents, boolean ignore_children) {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("id", getId());
         jsonObject.put("bezeichnung", getBezeichnung());
-        if (getParent() == null) {
-            jsonObject.put("parent", JSONObject.NULL);
-        } else {
-            jsonObject.put("parent", getParent().toJSONObject());
+
+        if (!ignore_parents) {
+            if (getParent() == null) {
+                jsonObject.put("parent", JSONObject.NULL);
+            } else {
+                jsonObject.put("parent", getParent().toJSONObject(false, true));
+            }
         }
 
-        /*
-        // This section is not used in JSON and breaks the functionality,
-        // so we disable it for now
-        JSONArray childArray = new JSONArray();
-        for (SelektionHierarchy child : getChildren()) {
-            childArray.put(child.toJSONObject());
+        if (!ignore_children) {
+            JSONArray childArray = new JSONArray();
+            for (SelektionHierarchy child : getChildren()) {
+                childArray.put(child.toJSONObject(true, false));
+            }
+            jsonObject.put("children", childArray);
         }
-        jsonObject.put("children", childArray);
-        */
 
         return jsonObject;
+    }
+
+    public JSONObject toJSONObject() {
+        return toJSONObject(false, false);
     }
 }
