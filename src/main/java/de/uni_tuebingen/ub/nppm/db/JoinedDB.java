696808f4dd69a65e28be7ad3b199b128edeca1e2
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-08-30 10:36:06 +0200

    inc.modul.jsp refactoring (use DB class instead of direct DB connection)

---
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/JoinedDB.java
@@ -0,0 +1,10 @@
+package de.uni_tuebingen.ub.nppm.db;
+
+/**
+ * Used as base for all kinds of joined queries,
+ * so we don't have to use AbstractBase directly.
+ * Maybe we find a way to add useful functions later.
+ */
+public class JoinedDB extends AbstractBase {
+
+}
