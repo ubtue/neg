f28a9815579357321a0d33245e60515815cad2bc
rnaiser <ryan.naiser@uni-tuebingen.de>
2023-09-14 16:12:55 +0200

    SelektonDB updated

--- src/main/java/de/uni_tuebingen/ub/nppm/db/SelektionDB.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/db/SelektionDB.java
@@ -2,6 +2,7 @@ package de.uni_tuebingen.ub.nppm.db;
 
 import static de.uni_tuebingen.ub.nppm.db.AbstractBase.getSession;
 import de.uni_tuebingen.ub.nppm.model.*;
+import java.math.BigInteger;
 import java.util.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -12,16 +13,16 @@ import org.hibernate.query.NativeQuery;
 import org.json.*;
 
 /**
- * Class for functions which can be applied to all Selektion...- tables
- * in a similar way
+ * Class for functions which can be applied to all Selektion...- tables in a
+ * similar way
  */
 public class SelektionDB extends AbstractBase {
 
-    static protected Map<String,String> gastToNonGast = Map.ofEntries(
-        Map.entry("gastselektion_amtweihe_einzelbeleg", "selektion_amtweihe"),
-        Map.entry("gastselektion_amtweihe_person", "selektion_amtweihe"),
-        Map.entry("gastselektion_quellengattung", "selektion_quellengattung"),
-        Map.entry("gastselektion_stand", "selektion_stand")
+    static protected Map<String, String> gastToNonGast = Map.ofEntries(
+            Map.entry("gastselektion_amtweihe_einzelbeleg", "selektion_amtweihe"),
+            Map.entry("gastselektion_amtweihe_person", "selektion_amtweihe"),
+            Map.entry("gastselektion_quellengattung", "selektion_quellengattung"),
+            Map.entry("gastselektion_stand", "selektion_stand")
     );
 
     static public List<Selektion> getList(String selektion) throws Exception {
@@ -31,7 +32,7 @@ public class SelektionDB extends AbstractBase {
     static public List<SelektionHierarchy> getListHierarchy(String selektion) throws Exception {
         Class c = getEntityClassByTableName(selektion);
 
-        try (Session session = getSession()) {
+        try ( Session session = getSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery query = builder.createQuery(c);
             Root root = query.from(c);
@@ -58,13 +59,13 @@ public class SelektionDB extends AbstractBase {
     static public SelektionBezeichnung getByBezeichnung(String selektion, String Bezeichnung) throws Exception {
         Class c = getEntityClassByTableName(selektion);
 
-        try (Session session = getSession()) {
+        try ( Session session = getSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery query = builder.createQuery(c);
             Root root = query.from(c);
             query.select(root).where(builder.equal(root.get(SelektionBezeichnung_.BEZEICHNUNG), Bezeichnung));
             try {
-                return (SelektionBezeichnung)session.createQuery(query).getSingleResult();
+                return (SelektionBezeichnung) session.createQuery(query).getSingleResult();
             } catch (NoResultException e) {
                 return null;
             }
@@ -72,12 +73,19 @@ public class SelektionDB extends AbstractBase {
     }
 
     public static List<Object> getBezeichnung(String tabelle, String bezeichnung) throws Exception {
-        try (Session session = getSession()) {
-            String SQL = "SELECT Bezeichnung FROM " + tabelle + " WHERE Bezeichnung='" + bezeichnung + "'";
-            NativeQuery query = session.createNativeQuery(SQL);
+        List<Object> results = new ArrayList<>();
+
+        try ( Session session = getSession()) {
+            session.getTransaction().begin();
+
+            String sql = "SELECT Bezeichnung FROM " + tabelle + " WHERE Bezeichnung= :bezeichnung";
+            NativeQuery query = session.createNativeQuery(sql);
+            query.setParameter("bezeichnung", bezeichnung);
             List<Object> rows = query.getResultList();
-            return rows;
+            results.addAll(rows);
+            session.getTransaction().commit();
         }
+        return results;
     }
 
     public static boolean hasBezeichnung(String selektion, String Bezeichnung) throws Exception {
@@ -85,15 +93,34 @@ public class SelektionDB extends AbstractBase {
     }
 
     public static void insertBezeichnung(String tabelle, String bezeichnung) throws Exception {
-        String sql = "INSERT INTO "+tabelle+" (Bezeichnung) VALUES (\""+bezeichnung+"\")";
-        insertOrUpdate(sql);
+        try ( Session session = getSession()) {
+            session.getTransaction().begin();
+
+            // Zuerst die maximale ID abrufen und um eins erhöhen
+            BigInteger maxId = (BigInteger) session.createNativeQuery("SELECT MAX(ID) + 1 FROM " + tabelle).uniqueResult();
+            if (maxId == null) {
+                maxId = BigInteger.ONE; // Wenn keine Daten vorhanden sind, beginnen Sie mit ID 1.
+            }
+
+            // Den INSERT-Befehl mit der generierten ID ausführen
+            NativeQuery query = session.createNativeQuery("INSERT INTO " + tabelle + " (ID, Bezeichnung) VALUES (:id, :bezeichnung)");
+            query.setParameter("id", maxId);
+            query.setParameter("bezeichnung", bezeichnung);
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        }
     }
 
     public static void updateBezeichnung(String tabelle, String bezeichnung, String id) throws Exception {
-        String sql = "UPDATE "+tabelle
-                        +" SET Bezeichnung=\""+bezeichnung+"\""
-                        +" WHERE ID="+id;
-        insertOrUpdate(sql);
+        try ( Session session = getSession()) {
+            session.getTransaction().begin();
+            NativeQuery query = session.createNativeQuery("UPDATE " + tabelle + " SET Bezeichnung= :bezeichnung WHERE ID= :id");
+            query.setParameter("bezeichnung", bezeichnung);
+            query.setParameter("id", id);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     static public boolean isHierarchy(String selektion) {
@@ -109,8 +136,9 @@ public class SelektionDB extends AbstractBase {
 
     static public void updateParentId(String selektion, int id, Integer parentId) throws Exception {
         String intVal = "NULL";
-        if (parentId != null)
+        if (parentId != null) {
             intVal = parentId.toString();
+        }
         String sql = "UPDATE " + selektion + " SET parentId=" + intVal + " WHERE ID=" + id;
         insertOrUpdate(sql);
     }
