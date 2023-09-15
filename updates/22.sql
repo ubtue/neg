a92ee7ae699ab5c3a76dfa4e0b77ee7d3870ea0c
jrost <julianrost@posteo.de>
2023-09-15 12:30:21 +0200

    Change Order

--- updates/22.sql
+++ updates/22.sql
@@ -1,12 +1,15 @@
+-- remove FK
 ALTER TABLE einzelbeleg DROP FOREIGN KEY einzelbeleg_QuelleGattungID;
 ALTER TABLE selektion_quellengattung DROP FOREIGN KEY selektion_quellengattung_parentId;
 
-ALTER TABLE einzelbeleg ADD CONSTRAINT einzelbeleg_QuelleGattungID FOREIGN KEY (QuelleGattungID) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
-
-ALTER TABLE selektion_quellengattung ADD CONSTRAINT selektion_quellengattung_parentId FOREIGN KEY (parentId) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
-
 -- this is necessary because the id column contains 0 values. Otherwise an error appears
 SET SESSION sql_mode = CONCAT(@@SQL_MODE, ',NO_AUTO_VALUE_ON_ZERO');
+-- Change ID Column
 ALTER TABLE selektion_quellengattung MODIFY COLUMN ID INT NOT NULL AUTO_INCREMENT;
 -- restore sql mode
 SET SESSION sql_mode=default;
+
+-- Add FK
+ALTER TABLE einzelbeleg ADD CONSTRAINT einzelbeleg_QuelleGattungID FOREIGN KEY (QuelleGattungID) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
+
+ALTER TABLE selektion_quellengattung ADD CONSTRAINT selektion_quellengattung_parentId FOREIGN KEY (parentId) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
