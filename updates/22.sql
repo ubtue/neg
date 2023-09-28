1116f46a38acc5aed61786ed14980ba9d84bb12a
rnaiser <ryan.naiser@uni-tuebingen.de>
2023-09-28 13:15:25 +0200

    again updates 22_sql

--- updates/22.sql
+++ updates/22.sql
@@ -1,4 +1,4 @@
-# The Dateiname field should be non-empty either a number or null later the programm will writh numbers or null inside but this old table and field has to be updated
+# The Dateiname field should be non-empty either a number or null later the programm will write numbers or null inside but this old table and field has to be updated
 
 UPDATE namenkommentar SET Dateiname = NULL WHERE Dateiname = '';
 
