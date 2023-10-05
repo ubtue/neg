3fd5956aacd2d94beccd1ff106186c244f17df68
jrost <julianrost@posteo.de>
2023-10-05 13:21:44 +0200

    Rename Field

--- updates/22.sql
+++ updates/22.sql
@@ -13,9 +13,9 @@ INSERT INTO `neg`.`selektion_kontext` (ID,Bezeichnung) VALUES (-1,'-');
 
 /*Create the datamapping for the kontext attribute of the einzelbeleg form*/
 INSERT INTO datenbank_mapping (Formular, Datenfeld, de_Beschriftung, Feldtyp, Array, ZielTabelle, ZielAttribut, Auswahlherkunft, Seite, gb_beschriftung, fr_beschriftung, la_beschriftung)
-VALUES ("einzelbeleg", "KontextSelektion", "Kontext Klassifikation", "select", 0, "einzelbeleg", "KontextID", "selektion_kontext", "einzelbeleg", "Context Classification", "Classification Du Contexte", "Context Genus");
+VALUES ("einzelbeleg", "KontextSelektion", "Kontext", "select", 0, "einzelbeleg", "KontextID", "selektion_kontext", "einzelbeleg", "Context", "Contexte", "Context");
 
 ALTER TABLE einzelbeleg ADD KontextID INT DEFAULT -1;
 
 /*Rename existing kontext Beschriftung*/
-UPDATE datenbank_mapping SET de_Beschriftung = 'Kontext Beschreibung', gb_beschriftung = 'Context Description', fr_beschriftung = 'Description Du Contexte', la_beschriftung = 'Contextus Description' WHERE Datenfeld = 'Kontext' AND Feldtyp = 'textarea' AND Formular = 'einzelbeleg';
+UPDATE datenbank_mapping SET de_Beschriftung = 'Textkritische Anmerkung', gb_beschriftung = 'Text-critical note', fr_beschriftung = 'Note critique sur le texte', la_beschriftung = 'Nota cum textu critico' WHERE Datenfeld = 'Kontext' AND Feldtyp = 'textarea' AND Formular = 'einzelbeleg';
