f3249627b338997004f392106470a79ad9622a87
jrost <julianrost@posteo.de>
2023-10-05 11:21:40 +0200

    Add Missing Fields

---
+++ updates/22.sql
@@ -0,0 +1,11 @@
+ALTER TABLE einzelbeleg ADD pal_abgrenzung VARCHAR(10) DEFAULT NULL;
+
+/*Create the datamapping for the pal_abgrenzung attribute of the einzelbeleg form*/
+INSERT INTO datenbank_mapping (Formular, Datenfeld, de_Beschriftung, Feldtyp, Array, ZielTabelle, ZielAttribut, Auswahlherkunft, Seite, gb_beschriftung, fr_beschriftung, la_beschriftung)
+VALUES ("einzelbeleg", "PalAbgrenzung", "Pal Abgrenzung", "textfield", 0, "einzelbeleg", "pal_abgrenzung", "", "einzelbeleg", "Pal Demarcation", "Pal Démarcation", "Pal Signatio");
+
+ALTER TABLE einzelbeleg ADD inh_abgrenzung VARCHAR(10) DEFAULT NULL;
+
+/*Create the datamapping for the inh_abgrenzung attribute of the einzelbeleg form*/
+INSERT INTO datenbank_mapping (Formular, Datenfeld, de_Beschriftung, Feldtyp, Array, ZielTabelle, ZielAttribut, Auswahlherkunft, Seite, gb_beschriftung, fr_beschriftung, la_beschriftung)
+VALUES ("einzelbeleg", "InhAbgrenzung", "Inh Abgrenzung", "textfield", 0, "einzelbeleg", "inh_abgrenzung", "", "einzelbeleg", "Inh Demarcation", "Inh Démarcation", "Inh Signatio");
