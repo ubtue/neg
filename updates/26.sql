ee922e91632ae2fd384cd4f67d7a870605a070f2
rnaiser <ryan.naiser@uni-tuebingen.de>
2023-11-13 14:18:36 +0100

    moved 26sql to 27sql

--- updates/26.sql
+++
@@ -1,1 +0,0 @@
-UPDATE selektion_areal SET Bezeichnung = 'D: Zarten, Baden-Württemberg, Landkr. Breisgau-Hochschwarzwald, Gem. Kirchzarten' WHERE ID = 2841;
---
+++ updates/27.sql
@@ -0,0 +1,2 @@
+UPDATE selektion_areal SET Bezeichnung = 'D: Zarten, Baden-Württemberg, Landkr. Breisgau-Hochschwarzwald, Gem. Kirchzarten' WHERE ID = 2841;
+
