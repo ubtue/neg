2be1a10ad18b42ff0ec3dd69d1d6f400afdaeabd
rnaiser <ryan.naiser@uni-tuebingen.de>
2023-12-20 10:31:12 +0100

    table ueberlieferung_edition update to ON DELETE CASCADE

--- updates/32.sql
+++ updates/32.sql
@@ -1,3 +1,13 @@
 INSERT INTO datenbank_mapping(Formular, Datenfeld, de_Beschriftung, Feldtyp, Array, Seite, gb_beschriftung, fr_beschriftung, la_beschriftung)  VALUES ("freie_suche", "Seite", "Seite", "textfield", 0, "freie_suche", "page", "page", "pagina");
 
-UPDATE datenbank_mapping SET de_Tooltip = 'Die NeG-ID besteht aus einem Buchstaben für die Art des Datensatzes (B=Einzelbeleg, E=Edition, M=MGH-Lemma, N=NamenLemma, P=Person, Q=Quelle) + laufender Nummer (z. b. P 7404 = Karl der Große). Geben Sie hier eine vollständige ID ein. Die ID finden Sie oben rechts in jedem Datensatz-Eintrag' WHERE ID = 276;   
+UPDATE datenbank_mapping SET de_Tooltip = 'Die NeG-ID besteht aus einem Buchstaben für die Art des Datensatzes (B=Einzelbeleg, E=Edition, M=MGH-Lemma, N=NamenLemma, P=Person, Q=Quelle) + laufender Nummer (z. b. P 7404 = Karl der Große). Geben Sie hier eine vollständige ID ein. Die ID finden Sie oben rechts in jedem Datensatz-Eintrag' WHERE ID = 276;
+
+ALTER TABLE ueberlieferung_edition
+	DROP FOREIGN KEY ueberlieferung_edition_EditionID,
+	ADD CONSTRAINT neu_ueberlieferung_edition_EditionID FOREIGN KEY (EditionID) REFERENCES edition (ID) ON DELETE CASCADE;  
+     
+
+
+ALTER TABLE ueberlieferung_edition
+	DROP FOREIGN KEY ueberlieferung_edition_UeberlieferungID,    
+     	ADD CONSTRAINT neu_ueberlieferung_edition_UeberlieferungID FOREIGN KEY (UeberlieferungID) REFERENCES handschrift_ueberlieferung (ID) ON DELETE CASCADE; 
