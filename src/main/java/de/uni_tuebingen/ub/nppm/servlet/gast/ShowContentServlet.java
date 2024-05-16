f46153b5328b29e4971ec6ff80c83e22e377ac6e
rnaiser <ryan.naiser@uni-tuebingen.de>
2024-05-16 11:51:17 +0200

    showContentServlet

--- src/main/java/de/uni_tuebingen/ub/nppm/servlet/gast/ShowContentServlet.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/servlet/gast/ShowContentServlet.java
@@ -8,29 +8,45 @@ import javax.servlet.http.HttpServletResponse;
 import de.uni_tuebingen.ub.nppm.db.*;
 import de.uni_tuebingen.ub.nppm.model.*;
 import java.io.OutputStream;
+import javax.servlet.http.Cookie;
 
 public class ShowContentServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        try {
-            Content content = ContentDB.getByName(name);
-            resp.setContentType(content.getContent_Type());
 
-            if (content.getContent_Type().startsWith("text/plain") || content.getContent_Type().startsWith("application/vnd.oasis.opendocument.text")
-                    || content.getContent_Type().startsWith("application/msword")
-                    || content.getContent_Type().startsWith("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))  {
-                // Set Content-Disposition header to specify the filename
-            resp.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
+        Cookie[] cookies = req.getCookies();
+        String selectedLanguage = "de";  //Standardwert wenn kein Cookie gesetzt worden ist
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("selectedLanguage")) {
+                    selectedLanguage = cookie.getValue();
+                    break;
+                }
             }
+        }
 
-            OutputStream os = resp.getOutputStream();
-            byte[] photoBytes = content.getContent();
-            os.write(photoBytes);
-            os.flush();
-            os.close();
+        try {
+            Content content = ContentDB.getByNameAndLanguage(name, selectedLanguage);
+            if (content != null) {
+                resp.setContentType(content.getContent_Type());
+
+                if (content.getContent_Type().startsWith("text/plain") || content.getContent_Type().startsWith("application/vnd.oasis.opendocument.text")
+                        || content.getContent_Type().startsWith("application/msword")
+                        || content.getContent_Type().startsWith("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
+                    // Set Content-Disposition header to specify the filename
+                    resp.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
+                }
+
+                OutputStream os = resp.getOutputStream();
+                byte[] photoBytes = content.getContent();
+                os.write(photoBytes);
+                os.flush();
+                os.close();
+            }
         } catch (Exception e) {
-            throw new ServletException(e);
+            resp.sendRedirect("file?context=CMS");
         }
     }
 }
