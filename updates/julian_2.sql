35a0613558182efeb18824c7da612ae79f4ed2f8
jrost <>
2024-05-23 16:04:02 +0200

    Rename MGH Lemma

--- updates/julian_2.sql
+++ updates/julian_2.sql
@@ -1,3 +1,11 @@
 UPDATE `datenbank_texte` SET `de` = 'Lemma', `gb` = 'lemma', `fr` = 'lemme', `la` = 'lemma' WHERE textfeld = 'Titel' AND formular = 'mgh_lemma';
 
+UPDATE `datenbank_texte` SET `de` = 'Lemma', `gb`='lemma', `fr`='lemme', `la`='lemma' WHERE Textfeld = 'OrderMGH' AND formular = 'freie_suche';
+
 UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma' WHERE Datenfeld = 'MGHLemma' AND formular = 'mgh_lemma';
+
+UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma' WHERE Datenfeld = 'MGHLemma' AND formular = 'freie_suche';
+
+UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma' WHERE Datenfeld = 'Ausgabe_MGHLemma' AND formular = 'freie_suche';
+
+UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma' WHERE Datenfeld = 'MGHLemmaRO' AND formular = 'einzelbeleg';
