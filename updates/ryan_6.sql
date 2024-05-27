d87280b23f334bba775434df1c3881b39fa1d21f
rnaiser <ryan.naiser@uni-tuebingen.de>
2024-05-27 17:08:07 +0200

    hilfe in CMS getan

---
+++ updates/ryan_6.sql
@@ -0,0 +1,7 @@
+UPDATE `neg`.`content` SET `context` = 'CMS', `language` = 'de' WHERE (`Bezeichnung` = 'hilfe.html');
+UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image001.png');
+UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image002.png');
+UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image003.png');
+UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image004.png');
+UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image005.png');
+UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image006.png');
