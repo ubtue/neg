5673097ca2579e708d3df76b7b1c619e1de60a00
rnaiser <ryan.naiser@uni-tuebingen.de>
2024-05-28 16:07:01 +0200

    Sprachauswahl tinyMce

--- updates/ryan_6.sql
+++ updates/ryan_6.sql
@@ -5,3 +5,7 @@ UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image003.pn
 UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image004.png');
 UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image005.png');
 UPDATE `neg`.`content` SET `context` = 'CMS' WHERE (`Bezeichnung` = 'image006.png');
+
+INSERT INTO `neg`.`datenbank_texte` (`Formular`, `Textfeld`, `de`, `gb`, `fr`, `la`) VALUES ('tinyMce', 'ButtonSpeichern', 'Speichern', 'Save', 'sauvegarder', 'Servare');
+
+INSERT INTO `neg`.`datenbank_texte` (`Formular`, `Textfeld`, `de`, `gb`, `fr`, `la`) VALUES ('tinyMce', 'LinkDateiBildVerwaltung', 'Datei & Bild Verwaltung', 'File & image management', 'Gestion des fichiers et des images', 'File & imaginem procuratio');
