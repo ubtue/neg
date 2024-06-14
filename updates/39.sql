a9115f3398c4f2b31ad00ac704e1d98463560e59
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-06-14 14:07:56 +0200

    39.sql: Add missing datenbank_text for context group

--- updates/39.sql
+++ updates/39.sql
@@ -2,6 +2,7 @@ INSERT INTO datenbank_mapping (Formular, Datenfeld, Feldtyp, Array, ZielTabelle,
 
 INSERT INTO datenbank_mapping (Formular, Datenfeld, Feldtyp, Array, ZielTabelle, ZielAttribut, gb_beschriftung, fr_beschriftung, de_Beschriftung, la_beschriftung) VALUES ("einzelbeleg", "Kontext_nach", "textarea", 0, "einzelbeleg", "Kontext_nach","context after", "contexte après", "Kontext nach", "postea context");
 
+INSERT INTO datenbank_texte (Formular, Textfeld, de, gb, fr, la) VALUES ("einzelbeleg", "Kontext", "Kontext", "Context", "Contexte", "Kontext");
 
 ALTER TABLE einzelbeleg ADD Kontext_vor mediumtext DEFAULT NULL AFTER Belegnummer;
 
