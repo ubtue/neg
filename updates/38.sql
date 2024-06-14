df561c4e14df94a8d493d9ec4061eac9eedf9888
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-06-14 13:02:01 +0200

    38.sql: Grouped statements, also renamed "Namenlemma" in advanced search

--- updates/38.sql
+++ updates/38.sql
@@ -1,19 +1,18 @@
-UPDATE `datenbank_texte` SET `de` = 'Lemma', `gb` = 'lemma', `fr` = 'lemme', `la` = 'lemma' WHERE textfeld = 'Titel' AND formular = 'mgh_lemma';
+UPDATE `datenbank_texte` SET `de` = 'Lemma', `gb` = 'lemma', `fr` = 'lemme', `la` = 'lemma'
+WHERE (textfeld = 'Titel' AND Formular = 'mgh_lemma')
+OR (Textfeld = 'OrderMGH' AND Formular = 'freie_suche');
 
-UPDATE `datenbank_texte` SET `de` = 'Lemma', `gb`='lemma', `fr`='lemme', `la`='lemma' WHERE Textfeld = 'OrderMGH' AND formular = 'freie_suche';
+UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma',`gb_Beschriftung` = 'Lemma',`fr_Beschriftung` = 'Lemme',`la_Beschriftung` = 'Lemma'
+WHERE (Datenfeld = 'MGHLemma' AND Formular = 'mgh_lemma')
+OR (Datenfeld = 'MGHLemma' AND Formular = 'freie_suche')
+OR (Datenfeld = 'Ausgabe_MGHLemma' AND Formular = 'freie_suche')
+OR (Datenfeld = 'MGHLemmaRO' AND Formular = 'einzelbeleg');
 
-UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma',`gb_Beschriftung` = 'Lemma',`fr_Beschriftung` = 'Lemme',`la_Beschriftung` = 'Lemma' WHERE Datenfeld = 'MGHLemma' AND formular = 'mgh_lemma';
+UPDATE datenbank_mapping SET de_combinedAnzeigenamen = 'Lemma;;Link',gb_combinedAnzeigenamen = 'Lemma;;Link',fr_combinedAnzeigenamen = 'Lemme;;Link',la_combinedAnzeigenamen = 'Lemma;;Link' WHERE Formular = 'einzelbeleg' AND Datenfeld = 'MGHLemma';
+UPDATE datenbank_mapping SET de_combinedAnzeigenamen = 'Philologisches Lemma;;Link',gb_combinedAnzeigenamen = 'Philological Lemma;;Link',fr_combinedAnzeigenamen = 'Lemme Philologique;;Link',la_combinedAnzeigenamen = 'Lemma Philologicum;;Link' WHERE Formular = 'einzelbeleg' AND Datenfeld = 'Lemma';
 
-UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma',`gb_Beschriftung` = 'Lemma',`fr_Beschriftung` = 'Lemme',`la_Beschriftung` = 'Lemma' WHERE Datenfeld = 'MGHLemma' AND formular = 'freie_suche';
-
-UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma',`gb_Beschriftung` = 'Lemma',`fr_Beschriftung` = 'Lemme',`la_Beschriftung` = 'Lemma' WHERE Datenfeld = 'Ausgabe_MGHLemma' AND formular = 'freie_suche';
-
-UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Lemma',`gb_Beschriftung` = 'Lemma',`fr_Beschriftung` = 'Lemme',`la_Beschriftung` = 'Lemma' WHERE Datenfeld = 'MGHLemmaRO' AND formular = 'einzelbeleg';
-
-UPDATE datenbank_mapping SET de_combinedAnzeigenamen = 'Lemma;;Link',gb_combinedAnzeigenamen = 'Lemma;;Link',fr_combinedAnzeigenamen = 'Lemme;;Link',la_combinedAnzeigenamen = 'Lemma;;Link' WHERE formular = 'einzelbeleg' AND Datenfeld = 'MGHLemma';
-
-UPDATE datenbank_mapping SET de_combinedAnzeigenamen = 'Philologisches Lemma;;Link',gb_combinedAnzeigenamen = 'Philological Lemma;;Link',fr_combinedAnzeigenamen = 'Lemme Philologique;;Link',la_combinedAnzeigenamen = 'Lemma Philologicum;;Link' WHERE formular = 'einzelbeleg' AND Datenfeld = 'Lemma';
-
-UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Philologisches Lemma',`gb_Beschriftung` = 'Philological lemma',`fr_Beschriftung` = 'Lemme philologique',`la_Beschriftung` = 'Lemma philologicum'  WHERE Datenfeld = 'PLemma' AND formular = 'namenkommentar';
-
-UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Philologisches Lemma',`gb_Beschriftung` = 'Philological lemma',`fr_Beschriftung` = 'Lemme philologique',`la_Beschriftung` = 'Lemma philologicum' WHERE Datenfeld = 'LemmaRO' AND formular = 'einzelbeleg';
+UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Philologisches Lemma',`gb_Beschriftung` = 'Philological lemma',`fr_Beschriftung` = 'Lemme philologique',`la_Beschriftung` = 'Lemma philologicum'
+WHERE (Datenfeld = 'PLemma' AND Formular = 'namenkommentar')
+OR (Datenfeld = 'LemmaRO' AND Formular = 'einzelbeleg')
+OR (Datenfeld = 'Namenlemma' AND Formular = 'freie_suche')
+OR (Datenfeld = 'Ausgabe_Namenlemma' AND Formular = 'freie_suche');
