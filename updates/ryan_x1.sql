6f0c459cf5b80c1829cab3965a3629a2f772908d
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-07-23 16:29:07 +0200

    ryan_x1.sql: formal corrections

--- updates/ryan_x1.sql
+++ updates/ryan_x1.sql
@@ -1,16 +1,11 @@
 -- Einträge einfügen
 INSERT INTO selektion_kritik (ID, Bezeichnung) VALUES (1, '?');
 
-ALTER TABLE `einzelbeleg`
-ADD COLUMN `TitelKritikID` int DEFAULT -1 NULL AFTER `KritikID`;
+INSERT INTO datenbank_mapping (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`, `FormularAttribut`, `Auswahlherkunft`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('einzelbeleg', 'TitelKritik', 'TitelKritik', 'select', '1', 'einzelbeleg_hattitelkritik', 'TitelKritikID', 'EinzelbelegID', 'selektion_kritik', 'einzelbeleg', 'Title critique', 'Critique de titre', 'Titulus Critica');
 
-INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`, `FormularAttribut`, `Auswahlherkunft`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('einzelbeleg', 'TitelKritik', 'TitelKritik', 'select', '1', 'einzelbeleg_hattitelkritik', 'TitelKritikID', 'EinzelbelegID', 'selektion_kritik', 'einzelbeleg', 'Title critique', 'Critique de titre', 'Titulus Critica');
+ALTER TABLE einzelbeleg ADD COLUMN TitelText varchar(255) DEFAULT NULL AFTER KontextID;
 
-
-ALTER TABLE einzelbeleg
-ADD COLUMN TitelText varchar(255) DEFAULT NULL AFTER TitelKritikID;
-
-INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('einzelbeleg', 'TitelText', 'Titel Text', 'textfield', '0', 'einzelbeleg', 'titelText', 'einzelbeleg', 'Title text', 'Texte du titre', 'Titulus textus');
+INSERT INTO datenbank_mapping (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('einzelbeleg', 'TitelText', 'Titel Text', 'textfield', '0', 'einzelbeleg', 'titelText', 'einzelbeleg', 'Title text', 'Texte du titre', 'Titulus textus');
 
 CREATE TABLE einzelbeleg_hattitelkritik (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -23,6 +18,4 @@ CREATE TABLE einzelbeleg_hattitelkritik (
 ALTER TABLE handschrift_ueberlieferung
 ADD COLUMN Schriftherkunft varchar(255) DEFAULT NULL AFTER Schriftheimat;
 
-
 UPDATE `neg`.`datenbank_mapping` SET `ZielAttribut` = 'QuelleID;;;Sigle;Schriftherkunft;Schriftheimat;Bibliotheksheimat;', `Auswahlherkunft` = ';;;;;selektion_ort;selektion_ort;', `combinedFeldnamen` = 'QuelleID;;;Sigle;Schriftherkunft;Schriftheimat;Bibliotheksheimat; ;', `combinedFeldtypen` = 'textfield;search(quelle,Bezeichnung,QuelleID);link(quelle,QuelleID,Bezeichnung);subtable;textfield;addselect;addselect;dateinfo', `de_combinedAnzeigenamen` = 'Quelle;;Link;Edition/Sigle;Schriftherkunft;Schriftheimat;Biblio.heimat;Datierung', `gb_combinedAnzeigenamen` = 'Source;;Link;Edition/Sigle;Origin of script;Origin of writing;Library origin;Dating', `fr_combinedAnzeigenamen` = 'source;;lien;édition/sigle;Origine de l\'écriture;lieu de création de l\'écriture;origine bibliothèque;datati', `la_combinedAnzeigenamen` = 'fons;;conexus;editio/sigla;Origo scripturae;origo libri;orig' WHERE Datenfeld = "Ueberlieferung";
-
