f57799aeeaa729d94db8b93348ad7e382b2fc4a1
jrost <>
2024-08-01 15:41:12 +0200

    Change Translation

--- updates/julian_2.sql
+++ updates/julian_2.sql
@@ -4,7 +4,7 @@ DELETE FROM datenbank_mapping WHERE Formular = 'einzelbeleg' AND Datenfeld = "Ar
 /*New Datenbank Mapping Entry*/
 INSERT INTO datenbank_mapping 
 (Formular,Datenfeld,Feldtyp,Array,ZielTabelle,ZielAttribut,FormularAttribut,Auswahlherkunft,combinedFeldnamen,combinedFeldtypen,Seite,de_beschriftung,gb_beschriftung,fr_beschriftung,la_beschriftung,de_combinedAnzeigenamen,la_combinedAnzeigenamen,gb_combinedAnzeigenamen,fr_combinedAnzeigenamen)
-VALUES ('einzelbeleg','NewAreal','combined',1,'einzelbeleg_hatareal','ArealID;ArealTypID','EinzelbelegID','selektion_areal;selektion_arealtyp','Areal;ArealTyp','select;select','einzelbeleg','Areal;ArealTyp','Areal;ArealTyp','Areal;ArealTyp','Areal;ArealTyp','Areal;ArealTyp','Areal;ArealTyp','Areal;ArealTyp','Areal;ArealTyp');
+VALUES ('einzelbeleg','NewAreal','combined',1,'einzelbeleg_hatareal','ArealID;ArealTypID','EinzelbelegID','selektion_areal;selektion_arealtyp','Areal;ArealTyp','select;select','einzelbeleg','Areal;ArealTyp','Areal;ArealTyp','Zone;ZoneTyp','Area;AreaTyp','Areal;ArealTyp','Area;AreaTyp','Areal;ArealTyp','Zone;ZoneTyp');
 
 
 ALTER TABLE `einzelbeleg_hatareal` ADD COLUMN `ArealTypID` int DEFAULT -1;
