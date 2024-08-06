291905f120eea16f964cd57be98e83dc4c3b6052
jrost <>
2024-08-06 10:42:43 +0200

    Rename Phil Lemma

---
+++ updates/julian_3.sql
@@ -0,0 +1,7 @@
+UPDATE datenbank_mapping SET de_combinedAnzeigenamen = 'Zusatznamenkommentar;;Link',gb_combinedAnzeigenamen = 'Additional name comment;;Link',fr_combinedAnzeigenamen = 'Commentaire sur le nom supplémentaire;;Link',la_combinedAnzeigenamen = 'Commentarium nominis additicius;;Link' WHERE Formular = 'einzelbeleg' AND Datenfeld = 'Lemma';
+
+UPDATE `datenbank_mapping` SET `de_Beschriftung` = 'Zusatznamenkommentar',`gb_Beschriftung` = 'Additional name comment',`fr_Beschriftung` = 'Commentaire sur le nom supplémentaire',`la_Beschriftung` = 'Commentarium nominis additicius'
+WHERE (Datenfeld = 'PLemma' AND Formular = 'namenkommentar')
+OR (Datenfeld = 'LemmaRO' AND Formular = 'einzelbeleg')
+OR (Datenfeld = 'Namenlemma' AND Formular = 'freie_suche')
+OR (Datenfeld = 'Ausgabe_Namenlemma' AND Formular = 'freie_suche');
