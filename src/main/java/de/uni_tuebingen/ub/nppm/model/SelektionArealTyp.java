032f8791de85862f85cc8ad3108cd36597471ee6
jrost <>
2024-08-06 08:45:32 +0200

    Rename Variable and Functions

--- src/main/java/de/uni_tuebingen/ub/nppm/model/SelektionArealTyp.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/model/SelektionArealTyp.java
@@ -7,21 +7,17 @@ import java.util.*;
 @Table(name = "selektion_arealtyp")
 public class SelektionArealTyp extends SelektionBezeichnung {
     @ManyToMany(mappedBy = "arealTyp")
-    private Set<Einzelbeleg> einzelbeleg = new HashSet<>();
+    private Set<Einzelbeleg> einzelbelege = new HashSet<>();
 
     public void addEinzelbeleg(Einzelbeleg beleg) {
-        this.getEinzelbeleg().add(beleg);
+        this.getEinzelbelege().add(beleg);
     }
 
     public void removeEinzelbeleg(int id) {
-        this.getEinzelbeleg().removeIf(e -> e.getId() == id);
+        this.getEinzelbelege().removeIf(e -> e.getId() == id);
     }
 
-    public Set<Einzelbeleg> getEinzelbeleg() {
-        return einzelbeleg;
-    }
-
-    public void setEinzelbeleg(Set<Einzelbeleg> einzelbeleg) {
-        this.einzelbeleg = einzelbeleg;
+    public Set<Einzelbeleg> getEinzelbelege() {
+        return einzelbelege;
     }
 }
