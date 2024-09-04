4bd201264a8563b812dce6b2a595660d072bd7f9
jrost <>
2024-09-04 09:17:49 +0200

    add sql update

---
+++ updates/julian_1.sql
@@ -0,0 +1,20 @@
+ALTER TABLE einzelbeleg_hattitelkritik ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE einzelbeleg_hattitelkritik ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
+
+ALTER TABLE person_hatgruppeherkunftareal ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE person_hatgruppeherkunftareal ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
+
+ALTER TABLE selektion_arealtyp ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE selektion_arealtyp ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
+
+ALTER TABLE selektion_beziehung_gemeinschaft ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE selektion_beziehung_gemeinschaft ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
+
+ALTER TABLE selektion_kontext ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE selektion_kontext ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
+
+ALTER TABLE selektion_kritik ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE selektion_kritik ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
+
+ALTER TABLE selektion_titelkritik ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
+ALTER TABLE selektion_titelkritik ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
