eb33959afde8a21ab47397e3c742e72f4d9dd679
jrost <>
2024-10-08 11:08:03 +0200

    Remove sql

--- updates/julian_3.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE selektion_kritik ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
-ALTER TABLE selektion_kritik ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
-
-ALTER TABLE gastselektion_amtweihe_einzelbeleg ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
-ALTER TABLE gastselektion_amtweihe_einzelbeleg ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
