c61e514ceb66cd18e10bdf3c4c042e2714b13277
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-10-11 11:06:11 +0200

    46.sql: remove duplicate columns einzelbeleg_hatfunktion, see 41.sql

--- updates/46.sql
+++ updates/46.sql
@@ -1,5 +1,2 @@
 ALTER TABLE einzelbeleg_hatangabe ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
 ALTER TABLE einzelbeleg_hatangabe ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
-
-ALTER TABLE einzelbeleg_hatfunktion ADD provenance_source VARCHAR (255) NOT NULL DEFAULT('NeG');
-ALTER TABLE einzelbeleg_hatfunktion ADD provenance_id VARCHAR (255) DEFAULT NULL, ADD CONSTRAINT unique_provenance_id UNIQUE (provenance_id);
