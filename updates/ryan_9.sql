60eec2cc4c711150475524d4f2c0c9ee289d57d0
rnaiser <ryan.naiser@uni-tuebingen.de>
2025-01-22 15:23:59 +0100

    Verbesserung gast Gross klein schreibung Headers

--- updates/ryan_9.sql
+++ updates/ryan_9.sql
@@ -51,3 +51,91 @@ UPDATE `neg`.`datenbank_texte` SET `gb` = 'Dating of the Text Attestor', `fr` = 
 UPDATE `neg`.`datenbank_texte` SET `gb` = 'Remark', `fr` = 'Remarque', `la` = 'Commentaria' where Formular = "einzelbeleg" and Textfeld = "Bemerkung";
 
 UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Text-critical Note' where Formular = "einzelbeleg" and Datenfeld = "KritikSelektion";
+
+UPDATE `neg`.`datenbank_texte` SET `la` = 'Numerus testimoniorum' where Formular = "stat" and Textfeld = "AnzahlBelege";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Person', `fr` = 'Personne', `la` = 'Persona' where Formular = "person" and Textfeld = "person";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Sex', `fr_beschriftung` = 'Sexe', `la_beschriftung` = 'Sexus' where Formular = "person" and Datenfeld = "Geschlecht";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Status', `fr_beschriftung` = 'Statut', `la_beschriftung` = 'Status' where Formular = "person" and Datenfeld = "Stand";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Offices' where Formular = "person" and Textfeld = "Aemter";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Ethnicity', `fr_beschriftung` = 'Ethnie', `la_beschriftung` = 'Gens' where Formular = "person" and Datenfeld = "Ethnie";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Relatives', `fr` = 'Parents', `la` = 'Cognati' where Formular = "person" and Textfeld = "TabVerwandte";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Reference', `fr` = 'Référence', `la` = 'Testimonia' where Formular = "person" and Textfeld = "TabEinzelbelege";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Name of the Person', `fr` = 'Nom de la personne', `la` = 'Nomen personae' where Formular = "gast_person" and Textfeld = "PersonName";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Degrees of Relationship', `fr` = 'Degré de parenté', `la` = 'Gradus propinquitatis' where Formular = "gast_person" and Textfeld = "Verwandtschaftsgrade";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_combinedAnzeigenamen` = 'Ethnie;Type of Attribution', `fr_combinedAnzeigenamen` = 'Ethnie;Type de l\'attribution', `la_combinedAnzeigenamen` = 'Gens;Modus attribuendi' where Formular = "person" and Datenfeld = "Ethnie";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_combinedAnzeigenamen` = 'Office;Period of Time;Identification', `fr_combinedAnzeigenamen` = 'Fonction/Ordination;Période;identification', `la_combinedAnzeigenamen` = 'Officium/Sacri ordines;Spatium temporis;identificatio' where Formular = "person" and Datenfeld = "AmtWeihe";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Reference', `fr` = 'Référence', `la` = 'Testimonium' where Formular = "person" and Textfeld = "Beleg";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Form of Reference', `fr` = 'Forme de la référence', `la` = 'Forma Testimonii' where Formular = "person" and Textfeld = "Belegform";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Dating', `fr` = 'Datation', `la` = 'Definitio temporis' where Formular = "person" and Textfeld = "Datierung";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Office/Consecration', `fr` = 'Fonction/Ordination', `la` = 'Officium/Consecratio' where Formular = "person" and Textfeld = "AmtWeihe";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Status', `fr` = 'Statut', `la` = 'Status' where Formular = "person" and Textfeld = "Stand";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Context', `fr` = 'Contexte', `la` = 'Contextus ' where Formular = "person" and Textfeld = "Kontext";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Reference', `fr` = 'Référence', `la` = 'Testimonium' where Formular = "mgh_lemma" and Textfeld = "Beleg";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Reference', `fr` = 'Référence', `la` = 'Testimonium' where Formular = "namenkommentar" and Textfeld = "Beleg";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'References', `fr_beschriftung` = 'Références', `la_beschriftung` = 'Testimonia' where Formular = "mgh_lemma" and Datenfeld = "EinzelbelegRO";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'References', `fr_beschriftung` = 'Références', `la_beschriftung` = 'Testimonia' where Formular = "namenkommentar" and Datenfeld = "EinzelbelegRO";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Dating', `fr` = 'Datation', `la` = 'Definitio temporis' where Formular = "quelle" and Textfeld = "Datierung";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Commentary (Dating)', `fr_beschriftung` = 'Commentaire (datation)', `la_beschriftung` = 'Commentarius datationis' where Formular = "quelle" and Datenfeld = "KommentarDatierung";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Series', `fr` = 'Série', `la` = 'Series' where Formular = "quelle" and Textfeld = "Reihe";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Location', `fr` = 'Lieu ', `la` = 'Locus' where Formular = "quelle" and Textfeld = "Ort";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Year', `fr` = 'Année', `la` = 'Annus' where Formular = "quelle" and Textfeld = "Jahr";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Pages', `fr` = 'Pages', `la` = 'Paginae' where Formular = "quelle" and Textfeld = "Seiten";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Standard Edition', `fr` = '  Édition standard', `la` = 'Editio Standard' where Formular = "quelle" and Textfeld = "StandardEdition";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Further Editions', `fr` = 'Éditions supplémentaires', `la` = 'Editiones alterae' where Formular = "quelle" and Textfeld = "WeitereEditionen";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Editor', `fr` = 'Éditeur', `la` = 'Editor' where Formular = "quelle" and Textfeld = "Herausgeber";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Single Reference' where Formular = "einzelbeleg" and Textfeld = "Titel";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Tradition', `fr` = 'Tradition', `la` = 'Traditio' where Formular = "quelle" and Textfeld = "TabUeberlieferung";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Signature/Indication', `fr` = 'Signature/Désignation', `la` = 'Signum libri/Inscriptio' where Formular = "quelle" and Textfeld = "Signatur";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Symbol', `fr` = 'Sigle', `la` = 'Sigla' where Formular = "quelle" and Textfeld = "Sigle";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Origin of the Writing', `fr` = 'Origine de l’écriture', `la` = 'Origo libri' where Formular = "quelle" and Textfeld = "Schriftheimat";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Edition', `fr` = 'Édition', `la` = 'Editio' where Formular = "quelle" and Textfeld = "Edition";
+
+UPDATE `neg`.`datenbank_texte` SET `gb` = 'Charter', `fr` = 'Chartes', `la` = 'Chartae' where Formular = "quelle" and Textfeld = "TabUrkunde";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Place of execution', `fr_beschriftung` = 'Lieu d\'exécution', `la_beschriftung` = 'Actum' where Formular = "urkunde" and Datenfeld = "Actumort";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Subject', `fr_beschriftung` = 'Objet', `la_beschriftung` = 'Titulus' where Formular = "urkunde" and Datenfeld = "Betreff";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Issuer of the Charter', `fr_beschriftung` = 'Auteur de la charte', `la_beschriftung` = 'Qui chartam subscripsit' where Formular = "urkunde" and Datenfeld = "Aussteller";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Recipient', `fr_beschriftung` = 'Destinataire', `la_beschriftung` = 'Destinatarius' where Formular = "urkunde" and Datenfeld = "Empfaenger";
+
+UPDATE `neg`.`datenbank_mapping` SET `gb_beschriftung` = 'Dorsal Note', `fr_beschriftung` = 'Note dorsale', `la_beschriftung` = 'Adnotata dorsualia' where Formular = "urkunde" and Datenfeld = "Dorsalnotiz";
+
+
