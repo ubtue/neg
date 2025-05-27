a52c32df2030c28741f253eea15ec0378f074086
jrost <>
2025-05-27 13:22:53 +0200

    Merge printPageNavigation function

--- src/main/java/de/uni_tuebingen/ub/nppm/util/suche/pagination/PrintPagination.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/util/suche/pagination/PrintPagination.java
@@ -1,94 +1,160 @@
 package de.uni_tuebingen.ub.nppm.util.suche.pagination;
 
+import de.uni_tuebingen.ub.nppm.db.EinzelbelegDB;
+import de.uni_tuebingen.ub.nppm.db.LemmaDB;
+import de.uni_tuebingen.ub.nppm.db.PersonDB;
+import de.uni_tuebingen.ub.nppm.db.QuelleDB;
 import de.uni_tuebingen.ub.nppm.util.Language;
 import static de.uni_tuebingen.ub.nppm.util.Utils.urlEncode;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspWriter;
 
 public class PrintPagination {
 
-    public static void printPageNavigation(JspWriter out, HttpServletRequest request, int pageoffset, int pageLimit, int linecount, String export) throws Exception {
+    // Hauptfunktion: erkennt Modus anhand von Parametern
+    public static void printPageNavigation(JspWriter out, HttpServletRequest request, Integer pageoffset, Integer pageLimit, Integer linecount, String export, String title) throws Exception {
         if (!"liste".equals(export) && !"browse".equals(export)) {
+            return;
+        }
+
+        List<Integer> publicIds = null;
+        Integer currentId = null;
+        int totalPages = 0;
+        int currentIndex = 0;
+        boolean useIdMode = false;
+
+        // Modus 1: Navigation nach IDs (title != null)
+        if (title != null) {
+            useIdMode = true;
+            switch (title) {
+                case "person":
+                    publicIds = PersonDB.getAllPublicPersonIds();
+                    break;
+                case "einzelbeleg":
+                    publicIds = EinzelbelegDB.getAllPublicEinzelbelegIds();
+                    break;
+                case "quelle":
+                    publicIds = QuelleDB.getAllPublicQuellenIds();
+                    break;
+                case "mgh_lemma":
+                    publicIds = LemmaDB.getAllPublicLemmaIds();
+                    break;
+                default:
+                    return;
+            }
+            totalPages = publicIds.size();
+            try {
+                currentId = Integer.parseInt(request.getParameter("ID"));
+            } catch (Exception e) {
+                currentId = publicIds.get(0);
+            }
+            currentIndex = publicIds.indexOf(currentId);
+            if (currentIndex == -1) {
+                currentIndex = 0;
+            }
+        } // Modus 2: Navigation nach Seitenoffset
+        else if (pageoffset != null && pageLimit != null && linecount != null) {
+            totalPages = (linecount + pageLimit - 1) / pageLimit;
+            currentIndex = pageoffset;
+        } else {
             return;
         }
 
         out.println("<div class=\"resultlistnavigation\" align=\"center\">");
 
-        int pages = (linecount + pageLimit - 1) / pageLimit;
-
         // Previous Button
-        if (pageoffset > 0) {
-            String prevUrl = "?pageoffset=" + (pageoffset - 1);
-            for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
-                String paramName = e.nextElement();
-                if (!paramName.equals("pageoffset")) {
-                    prevUrl += "&" + paramName + "=" + urlEncode(request.getParameter(paramName));
-                }
+        if (currentIndex > 0) {
+            if (useIdMode) {
+                int prevID = publicIds.get(currentIndex - 1);
+                out.print("<button class=\"ut-btn ut-btn--color-primary-3 prev-button\" onclick=\"window.location.href='?ID=" + prevID + "';\">Previous</button>&nbsp;");
+            } else {
+                String prevUrl = buildPageUrl(request, currentIndex - 1);
+                String prev = Language.getTextfield(request.getSession(), "pagination", "Prev");
+                out.print("<button class=\"ut-btn ut-btn--color-primary-3 prev-button\" onclick=\"window.location.href='" + prevUrl + "';\">" + prev + "</button>&nbsp;");
             }
-            String prev = Language.getTextfield(request.getSession(), "pagination", "Prev");
-            out.print("<button class=\"ut-btn ut-btn--color-primary-3 prev-button\" onclick=\"window.location.href='" + prevUrl + "';\">" + prev + "</button>&nbsp;");
         }
 
         // Page Number Buttons
-        for (int i = 0; i < pages; i++) {
-            if (i == 0 && i <= pageoffset - 10) {
-                String pageUrl = "?pageoffset=" + i;
-                for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
-                    String paramName = e.nextElement();
-                    if (!paramName.equals("pageoffset")) {
-                        pageUrl += "&" + paramName + "=" + urlEncode(request.getParameter(paramName));
-                    }
+        for (int i = 0; i < totalPages; i++) {
+            boolean showFirstDots = i == 0 && i <= currentIndex - 10;
+            boolean showLastDots = i == totalPages - 1 && i >= currentIndex + 10;
+            boolean inWindow = i < currentIndex + 10 && i > currentIndex - 10;
+
+            if (showFirstDots) {
+                if (useIdMode) {
+                    int pageID = publicIds.get(i);
+                    out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='?ID=" + pageID + "';\">1</button>&nbsp;...&nbsp;");
+                } else {
+                    String pageUrl = buildPageUrl(request, i);
+                    out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='" + pageUrl + "';\">1</button>&nbsp;...&nbsp;");
                 }
-                out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='" + pageUrl + "';\">1</button>&nbsp;...&nbsp;");
             }
 
-            if (i < pageoffset + 10 && i > pageoffset - 10) {
-                if (i == pageoffset) {
+            if (inWindow) {
+                if (i == currentIndex) {
                     out.print("<button class=\"ut-btn ut-btn--color-primary-1 current-button\" disabled>");
                     out.print((i + 1));
                     out.print("</button>&nbsp;");
                 } else {
-                    String pageUrl = "?pageoffset=" + i;
-                    for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
-                        String paramName = e.nextElement();
-                        if (!paramName.equals("pageoffset")) {
-                            pageUrl += "&" + paramName + "=" + urlEncode(request.getParameter(paramName));
-                        }
+                    if (useIdMode) {
+                        int pageID = publicIds.get(i);
+                        out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='?ID=" + pageID + "';\">" + (i + 1) + "</button>&nbsp;");
+                    } else {
+                        String pageUrl = buildPageUrl(request, i);
+                        out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='" + pageUrl + "';\">" + (i + 1) + "</button>&nbsp;");
                     }
-                    out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='" + pageUrl + "';\">");
-                    out.print((i + 1));
-                    out.print("</button>&nbsp;");
                 }
             }
 
-            if (i == pages - 1 && i >= pageoffset + 10) {
-                String pageUrl = "?pageoffset=" + i;
-                for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
-                    String paramName = e.nextElement();
-                    if (!paramName.equals("pageoffset")) {
-                        pageUrl += "&" + paramName + "=" + urlEncode(request.getParameter(paramName));
-                    }
+            if (showLastDots) {
+                if (useIdMode) {
+                    int pageID = publicIds.get(i);
+                    out.print("...&nbsp;<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='?ID=" + pageID + "';\">" + (i + 1) + "</button>&nbsp;");
+                } else {
+                    String pageUrl = buildPageUrl(request, i);
+                    out.print("...&nbsp;<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='" + pageUrl + "';\">" + (i + 1) + "</button>&nbsp;");
                 }
-                out.print("...&nbsp;<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='" + pageUrl + "';\">");
-                out.print((i + 1));
-                out.print("</button>&nbsp;");
             }
         }
 
         // Next Button
-        if (pageoffset < pages - 1) {
-            String nextUrl = "?pageoffset=" + (pageoffset + 1);
-            for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
-                String paramName = e.nextElement();
-                if (!paramName.equals("pageoffset")) {
-                    nextUrl += "&" + paramName + "=" + urlEncode(request.getParameter(paramName));
-                }
+        if (currentIndex < totalPages - 1) {
+            if (useIdMode) {
+                int nextID = publicIds.get(currentIndex + 1);
+                out.print("<button class=\"ut-btn ut-btn--color-primary-3 next-button\" onclick=\"window.location.href='?ID=" + nextID + "';\">Next</button>");
+            } else {
+                String nextUrl = buildPageUrl(request, currentIndex + 1);
+                String next = Language.getTextfield(request.getSession(), "pagination", "Next");
+                out.print("<button class=\"ut-btn ut-btn--color-primary-3 next-button\" onclick=\"window.location.href='" + nextUrl + "';\">" + next + "</button>");
             }
-            String next = Language.getTextfield(request.getSession(), "pagination", "Next");
-            out.print("<button class=\"ut-btn ut-btn--color-primary-3 next-button\" onclick=\"window.location.href='" + nextUrl + "';\">" + next + "</button>");
         }
 
         out.println("</div>");
+    }
+
+    // Komfortüberladung: klassischer Seitenmodus
+    public static void printPageNavigation(JspWriter out, HttpServletRequest request, int pageoffset, int pageLimit, int linecount, String export) throws Exception {
+        printPageNavigation(out, request, pageoffset, pageLimit, linecount, export, null);
+    }
+
+    // Komfortüberladung: klassischer ID-Modus
+    public static void printPageNavigation(JspWriter out, HttpServletRequest request, String export, String title) throws Exception {
+        printPageNavigation(out, request, null, null, null, export, title);
+    }
+
+    // Hilfsfunktion: baut Seiten-URL basierend auf Request und Ziel-Seitennummer
+    private static String buildPageUrl(HttpServletRequest request, int pageoffset) {
+        StringBuilder url = new StringBuilder("?pageoffset=" + pageoffset);
+        for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
+            String paramName = e.nextElement();
+            if (!paramName.equals("pageoffset")) {
+                url.append("&").append(paramName).append("=").append(URLEncoder.encode(request.getParameter(paramName), StandardCharsets.UTF_8));
+            }
+        }
+        return url.toString();
     }
 }
--- src/main/webapp/gast/layout/titel.inc.jsp
+++ src/main/webapp/gast/layout/titel.inc.jsp
@@ -1,79 +1,9 @@
+<%@page import="de.uni_tuebingen.ub.nppm.util.suche.pagination.PrintPagination"%>
 <%@page import="java.io.IOException"%>
 <%@page import="java.util.List"%>
 <%@page import="de.uni_tuebingen.ub.nppm.db.*"%>
 <%@page import="de.uni_tuebingen.ub.nppm.util.Language"%>
 <%@ include file="../../configuration.jsp" %>
-
-<%!
-public void printPageNavigation(JspWriter out, HttpServletRequest request, int pageoffset, int pageLimit, int linecount, String export, String title) throws Exception, IOException {
-    if (!"liste".equals(export) && !"browse".equals(export)) return;
-
-    List<Integer> publicIds;
-    if ("person".equals(title)) {
-        publicIds = PersonDB.getAllPublicPersonIds();
-    } else if("einzelbeleg".equals(title)) {
-        publicIds = EinzelbelegDB.getAllPublicEinzelbelegIds();
-    } else if("quelle".equals(title)) {
-        publicIds = QuelleDB.getAllPublicQuellenIds();
-    }  else if("mgh_lemma".equals(title)) {
-        publicIds = LemmaDB.getAllPublicLemmaIds();
-    }
-    else {
-        return;
-    }
-
-    int totalPages = publicIds.size();
-    int currentId = Integer.parseInt(request.getParameter("ID"));
-
-    // Finde die aktuelle Position in der Liste
-    int currentIndex = publicIds.indexOf(currentId);
-    if (currentIndex == -1) currentIndex = 0;
-
-    out.println("<div class=\"resultlistnavigation\" align=\"center\">");
-
-    // Previous
-    if (currentIndex > 0) {
-        int prevID = publicIds.get(currentIndex - 1);
-        out.print("<button class=\"ut-btn ut-btn--color-primary-3 prev-button\" onclick=\"window.location.href='?ID=" + prevID + "';\">Previous</button>&nbsp;");
-    }
-
-    // Page numbers
-    for (int i = 0; i < totalPages; i++) {
-        if (i == 0 && i <= currentIndex - 10) {
-            int pageID = publicIds.get(i);
-            out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='?ID=" + pageID + "';\">1</button>&nbsp;...&nbsp;");
-        }
-
-        if (i < currentIndex + 10 && i > currentIndex - 10) {
-            int pageID = publicIds.get(i);
-            if (i == currentIndex) {
-                out.print("<button class=\"ut-btn ut-btn--color-primary-1 current-button\" disabled>");
-                out.print((i + 1));
-                out.print("</button>&nbsp;");
-            } else {
-                out.print("<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='?ID=" + pageID + "';\">");
-                out.print((i + 1));
-                out.print("</button>&nbsp;");
-            }
-        }
-
-        if (i == totalPages - 1 && i >= currentIndex + 10) {
-            int pageID = publicIds.get(i);
-            out.print("...&nbsp;<button class=\"ut-btn ut-btn--color-primary-2 page-button\" onclick=\"window.location.href='?ID=" + pageID + "';\">");
-            out.print((i + 1));
-            out.print("</button>&nbsp;");
-        }
-    }
-
-    // Next
-    if (currentIndex < totalPages - 1) {
-        int nextID = publicIds.get(currentIndex + 1);
-        out.print("<button class=\"ut-btn ut-btn--color-primary-3 next-button\" onclick=\"window.location.href='?ID=" + nextID + "';\">Next</button>");
-    }
-
-    out.println("</div>");
-}
-%>
 
 <%    int id = -1;
     String title = "gast_" + request.getParameter("title").toLowerCase();
@@ -130,5 +60,5 @@ public void printPageNavigation(JspWriter out, HttpServletRequest request, int p
 
     String export = "browse";
 
-    printPageNavigation(out, request, pageoffset, pageLimit, max, export, title);
+    PrintPagination.printPageNavigation(out, request, pageoffset, pageLimit, max, export, title);
 %>
