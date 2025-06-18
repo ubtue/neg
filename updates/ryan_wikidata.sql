25a3fa231e3727708b731b382bf203b3ae94ce06
rnaiser <ryan.naiser@uni-tuebingen.de>
2025-06-18 10:50:12 +0200

    Erweiterung der Erweiterten Suche

--- updates/ryan_wikidata.sql
+++ updates/ryan_wikidata.sql
@@ -5,3 +5,17 @@ INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung
 INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`) VALUES ('person', 'WikidataLink', 'wikidatalink', '0', 'person', 'Wikidata');
 
 UPDATE `neg`.`datenbank_mapping` SET `Datenfeld` = 'IconLink' WHERE Formular = "person" and Datenfeld = "GNDLink";
+
+INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('freie_suche', 'Ausgabe_Einzelbeleg_AmtWeihe', 'Ämter und Weihegrade im Einzelbeleg', 'checkbox', '0', 'freie_suche', 'Offices and Degree of Ordination at single reference', 'Fonctions et degré d''ordination en référence individuelle', 'Officium et gradus ordinationis in testimonio');
+
+INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `Auswahlherkunft`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`, `de_Tooltip`, `gb_Tooltip`, `fr_Tooltip`, `la_Tooltip`) VALUES ('gast_freie_suche', 'StandEinzelbeleg', 'Stand (Einzelbeleg)', 'select', '0', 'selektion_stand', 'freie_suche', 'status (Single Reference)', 'Statut (référence indivuelle)', 'Status (testimonium)', 'Wählen Sie aus der Ausklappliste', 'Select the status from the drop-down menu', 'Select the status from the drop-down menu', 'Select the status from the drop-down menu');
+
+INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('freie_suche', 'Ausgabe_Stand_Einzelbeleg', 'Stand im Einzelbeleg', 'checkbox', '0', 'freie_suche', 'Status  at single reference', 'Statut en référence individuelle', 'Status in testimonio');
+
+UPDATE `neg`.`datenbank_mapping` SET `fr_beschriftung` = 'Fonction/Ordination (référence indivuelle)' WHERE Formular = "freie_suche" and Datenfeld = "AmtWeiheEinzelbeleg";
+
+UPDATE `neg`.`datenbank_mapping` SET `de_Beschriftung` = 'Stand Person', `gb_beschriftung` = 'Status Person', `fr_beschriftung` = 'Statut Personne', `la_beschriftung` = 'Status Persona' WHERE Formular = "freie_suche" and Datenfeld = "Ausgabe_Stand";
+
+INSERT INTO `neg`.`datenbank_texte` (`Formular`, `Textfeld`, `de`, `gb`, `fr`, `la`) VALUES ('freie_suche', 'OrderStandEinzelbeleg', 'Stand Einzelbeleg', 'Status (Single Reference)', 'Statut (référence indivuelle)', 'Status (testimonium)');
+
+INSERT INTO `neg`.`datenbank_texte` (`Formular`, `Textfeld`, `de`, `gb`, `fr`, `la`) VALUES ('freie_suche', 'OrderAmtWeiheEinzelbeleg', 'Amt/Weihe Einzelbeleg', 'Office/Consecration (Single Reference)', 'Fonction/Ordination (référence indivuelle)', 'Officium/Consecratio (testimonium)');
