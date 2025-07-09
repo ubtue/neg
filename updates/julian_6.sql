ac30431371aac676f51e4a39815ccf8b52bc552b
jrost <>
2025-07-09 16:24:46 +0200

    Add Migration Function

--- updates/julian_6.sql
+++ updates/julian_6.sql
@@ -5,3 +5,14 @@ ALTER TABLE quelle ADD CONSTRAINT fk_quelle_quellengattungid FOREIGN KEY (Quelle
 #ALTER TABLE einzelbeleg DROP COLUMN QuelleGattungID;
 
 INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`, `Auswahlherkunft`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('quelle', 'Quellengattung', 'Quellengattung', 'select', '0', 'quelle', 'QuelleGattungID', 'selektion_quellengattung', 'quelle', 'Source type', 'Type de source', 'Typus fontis');
+
+#Voraussetzung für die folgende Migration ist das jede Quelle nur eine Gattung hat
+#Dies muss vorher noch mit dem Fachbereich abgesprochen werden und dann bereinigt werden
+UPDATE quelle q
+JOIN (
+  SELECT QuelleID, MAX(QuelleGattungID) AS QuelleGattungID
+  FROM einzelbeleg
+  WHERE QuelleGattungID IS NOT NULL
+  GROUP BY QuelleID
+) e ON q.ID = e.QuelleID
+SET q.QuelleGattungID = e.QuelleGattungID;
