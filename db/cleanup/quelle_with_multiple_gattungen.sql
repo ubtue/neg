4da747454a3393e9160cd4be0ab55cd90d81f6fc
jrost <>
2025-07-23 14:00:37 +0200

    Fix SQL & einzelbeleg.jsp

--- db/cleanup/quelle_with_multiple_gattungen.sql
+++ db/cleanup/quelle_with_multiple_gattungen.sql
@@ -27,9 +27,6 @@ DROP TABLE bereinigung_csv;
 ALTER TABLE quelle ADD COLUMN QuelleGattungID INT NOT NULL DEFAULT '-1';
 ALTER TABLE quelle ADD CONSTRAINT fk_quelle_quellengattungid FOREIGN KEY (QuelleGattungID) REFERENCES selektion_quellengattung(ID) ON UPDATE CASCADE;
 
-#optional
-#ALTER TABLE einzelbeleg DROP COLUMN QuelleGattungID;
-
 INSERT INTO `neg`.`datenbank_mapping` (`Formular`, `Datenfeld`, `de_Beschriftung`, `Feldtyp`, `Array`, `ZielTabelle`, `ZielAttribut`, `Auswahlherkunft`, `Seite`, `gb_beschriftung`, `fr_beschriftung`, `la_beschriftung`) VALUES ('quelle', 'Quellengattung', 'Quellengattung', 'select', '0', 'quelle', 'QuelleGattungID', 'selektion_quellengattung', 'quelle', 'Source type', 'Type de source', 'Typus fontis');
 
 #migration
@@ -41,3 +38,6 @@ JOIN (
   GROUP BY QuelleID
 ) e ON q.ID = e.QuelleID
 SET q.QuelleGattungID = e.QuelleGattungID;
+
+#optional
+ALTER TABLE einzelbeleg DROP COLUMN QuelleGattungID;
