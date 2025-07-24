766d193442d185f6161c12134e985610603dd22e
jrost <>
2025-07-24 08:29:14 +0200

    Cleanup

--- updates/julian_1.sql
+++ updates/julian_1.sql
@@ -1,1 +1,2 @@
 UPDATE `neg`.`datenbank_mapping` SET `Feldtyp` = 'array.selektion.multiselect', `ZielTabelle` = '', `ZielAttribut` = '' WHERE (`Datenfeld` = 'Quellenliste' AND `Formular` = 'gast_freie_suche')
+DELETE FROM datenbank_mapping WHERE Formular = "einzelbeleg" AND Datenfeld = "Quellengattung";
