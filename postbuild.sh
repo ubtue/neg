3ab95f4672c659e808b68b96e705672574b1a34d
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-01-16 09:58:11 +0100

    postbuild.sh: Update jakartaee-migration to v1.0.10

--- postbuild.sh
+++ postbuild.sh
@@ -2,8 +2,8 @@
 
 # Auto-Migrate from Java EE to Jakarta EE
 TMP_DIR=/tmp
-JAR_URL_MIGRATION_TOOL="https://dlcdn.apache.org/tomcat/jakartaee-migration/v1.0.9/binaries/jakartaee-migration-1.0.9-shaded.jar"
-JAR_FILE_MIGRATION_TOOL="$TMP_DIR/jakartaee-migration-1.0.9-shaded.jar"
+JAR_URL_MIGRATION_TOOL="https://dlcdn.apache.org/tomcat/jakartaee-migration/v1.0.10/binaries/jakartaee-migration-1.0.10-shaded.jar"
+JAR_FILE_MIGRATION_TOOL="$TMP_DIR/jakartaee-migration-1.0.10-shaded.jar"
 WAR_FILE_TOMCAT="/var/lib/tomcat10/webapps/nppm.war"
 WAR_FILE_TMP_JAVAEE="$TMP_DIR/nppm.javaee.war"
 WAR_FILE_TMP_JAKARTAEE="$TMP_DIR/nppm.jakartaee.war"
