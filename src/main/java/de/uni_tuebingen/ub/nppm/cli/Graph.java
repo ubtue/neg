1a8a2ec9e82493f525c986f73e39ac09de34ed2a
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-04-23 14:31:07 +0200

    Graph.java: Add weights based on Seite/Raster

--- src/main/java/de/uni_tuebingen/ub/nppm/cli/Graph.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/cli/Graph.java
@@ -1,8 +1,8 @@
 package de.uni_tuebingen.ub.nppm.cli;
 
 import de.uni_tuebingen.ub.nppm.db.*;
-import org.jgrapht.graph.DefaultDirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
+import de.uni_tuebingen.ub.nppm.model.*;
+import org.jgrapht.graph.*;
 import org.jgrapht.nio.*;
 import org.jgrapht.nio.csv.*;
 import org.jgrapht.nio.dot.*;
@@ -17,6 +17,8 @@ import java.util.Map;
 public class Graph extends AbstractBase {
     // CLI Arguments
     private static Path outputPath;
+
+    private static org.jgrapht.Graph<String, DefaultWeightedEdge> graph;
 
     /**
      * Generate Graph-related data, e.g. for Gephi or Graphviz
@@ -40,33 +42,77 @@ public class Graph extends AbstractBase {
         System.exit(0);
     }
 
+    private static void addVertex(Person person) {
+        if (!graph.containsVertex(person.getPersistentIdentifier())) {
+            graph.addVertex(person.getPersistentIdentifier());
+        }
+    }
+
+    private static void addEdge(Person person1, Person person2, double weight) {
+        // quick & dirty workaround in case we run into non-public persons for some reason (should never happen!)
+        addVertex(person1);
+        addVertex(person2);
+
+        // Try to find existing edge (bidirectional / undirected), increment weight if possible
+        DefaultWeightedEdge edge = null;
+        edge = graph.getEdge(person1.getPersistentIdentifier(), person2.getPersistentIdentifier());
+        if (edge == null) {
+            edge = graph.getEdge(person2.getPersistentIdentifier(), person1.getPersistentIdentifier());
+        }
+        if (edge != null) {
+            graph.setEdgeWeight(edge, graph.getEdgeWeight(edge) + weight);
+        } else {
+            // or insert new edge if non exists
+            edge = graph.addEdge(person1.getPersistentIdentifier(), person2.getPersistentIdentifier());
+            graph.setEdgeWeight(edge, weight);
+        }
+    }
+
     private static void export() throws Exception {
         Log("Baue Graph...");
-        org.jgrapht.Graph<String, DefaultEdge> graph =
-                new DefaultDirectedGraph<>(DefaultEdge.class);
+        graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 
         // Note: We need to execute multi-pass, else vertices will not be found when creating edges
         // first: create all nodes (vertices)
         Log("Ermittle Knoten...");
         for (var person : PersonDB.getListPersonPublic()) {
-            graph.addVertex(person.getPersistentIdentifier());
+            addVertex(person);
         }
 
         // - second: create all edges
         Log("Ermittle Kanten...");
         for (var person : PersonDB.getListPersonPublic()) {
             for (var beleg : person.getEinzelbeleg()) {
-                // Keep it simple: Only relate personen within same einzelbeleg
-                if (beleg.getQuelle().getZuVeroeffentlichen().equals(1)) {
+                var quelle = beleg.getQuelle();
+                if (quelle.getZuVeroeffentlichen().equals(1)) {
+                    // Highest weight: Only relate personen within same einzelbeleg
                     for (var person2 : beleg.getPerson()) {
                         if (!person.getPersistentIdentifier().equals(person2.getPersistentIdentifier())) {
+                            addEdge(person, person2, 1);
+                        }
+                    }
 
-                            // quick & dirty workaround
-                            if (!graph.containsVertex(person2.getPersistentIdentifier())) {
-                                graph.addVertex(person2.getPersistentIdentifier());
+                    // Lower weight: Add personen from other einzelbelege on the same page (depending on raster if available)
+                    if (beleg.getSeite() != null) {
+                        for (var beleg2 : quelle.getEinzelbelege()) {
+                            if (!beleg.getPersistentIdentifier().equals(beleg2.getPersistentIdentifier())) {
+                                double weight = 1;
+                                if (beleg2.getSeite().equals(beleg.getSeite())) {
+                                    ++weight;
+
+                                    if (beleg.getRaster() != null && beleg.getRaster().equals(beleg2.getRaster()))  {
+                                        ++weight;
+                                    }
+                                    if (beleg.getSchreiber() != null && beleg.getSchreiber().equals(beleg2.getSchreiber()))  {
+                                        ++weight;
+                                    }
+                                }
+                                for (var person2 : beleg.getPerson()) {
+                                    if (!person2.getPersistentIdentifier().equals(person.getPersistentIdentifier())) {
+                                        addEdge(person, person2, weight);
+                                    }
+                                }
                             }
-
-                            graph.addEdge(person.getPersistentIdentifier(), person2.getPersistentIdentifier());
                         }
                     }
                 }
@@ -77,7 +123,7 @@ public class Graph extends AbstractBase {
             Log("Exportiere als CSV");
 
             // ADJACENCY_LIST for Gephi
-            CSVExporter<String, DefaultEdge> exporter =
+            CSVExporter<String, DefaultWeightedEdge> exporter =
             new CSVExporter<>(CSVFormat.ADJACENCY_LIST);
 
             // Vertex ID (important für CSV)
@@ -91,10 +137,18 @@ public class Graph extends AbstractBase {
             }
         } else if (outputPath.toString().endsWith(".dot")) {
             Log("Exportiere als DOT");
-            DOTExporter<String, DefaultEdge> exporter = new DOTExporter<>();
+            DOTExporter<String, DefaultWeightedEdge> exporter = new DOTExporter<>();
             exporter.setVertexAttributeProvider((v) -> {
                 Map<String, Attribute> map = new LinkedHashMap<>();
                 map.put("label", DefaultAttribute.createAttribute(v));
+                map.put("label", DefaultAttribute.createAttribute(v));
+                return map;
+            });
+            exporter.setEdgeAttributeProvider(e -> {
+                Map<String, Attribute> map = new LinkedHashMap<>();
+                double weight = graph.getEdgeWeight(e);
+                map.put("label", DefaultAttribute.createAttribute(weight));
+                map.put("weight", DefaultAttribute.createAttribute(weight)); // optional für DOT
                 return map;
             });
             try (Writer writer = new FileWriter(outputPath.toString())) {
@@ -102,7 +156,7 @@ public class Graph extends AbstractBase {
             }
         } else if (outputPath.toString().endsWith(".gexf")) {
             Log("Exportiere als GEXF");
-            GEXFExporter<String, DefaultEdge> exporter = new GEXFExporter<>();
+            GEXFExporter<String, DefaultWeightedEdge> exporter = new GEXFExporter<>();
             try (Writer writer = new FileWriter(outputPath.toString())) {
                 exporter.exportGraph(graph, writer);
             }
