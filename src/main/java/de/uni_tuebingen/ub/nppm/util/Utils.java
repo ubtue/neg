f19b0cd91bf48f04a365fa8c68760a3c009bc3e1
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-05-07 13:31:58 +0200

    Utils.java: Add proxy mode for getBaseUrl()

--- src/main/java/de/uni_tuebingen/ub/nppm/util/Utils.java
+++ src/main/java/de/uni_tuebingen/ub/nppm/util/Utils.java
@@ -78,13 +78,27 @@ public class Utils {
     }
 
     public static String getBaseUrl(HttpServletRequest request) {
-        String scheme = request.getScheme();
-        String host = request.getServerName();
-        int port = request.getServerPort();
-        String contextPath = request.getContextPath();
-
-        String baseUrl = scheme + "://" + host + ((("http".equals(scheme) && port == 80) || ("https".equals(scheme) && port == 443)) ? "" : ":" + port) + contextPath;
-        return baseUrl;
+        // Check whether we're behind a proxy
+        String xHost = request.getHeader("Host");
+        String xScheme = request.getHeader("X-Forwarded-Proto");
+        String xPort = request.getHeader("X-Forwarded-Port");
+        String xPrefix = request.getHeader("X-Forwarded-Prefix");
+        if (xScheme != null && xHost != null && xPort != null) {
+            // Use proxy configuration
+            String baseUrl = xScheme + "://" + xHost + ((("http".equals(xScheme) && xPort.equals("80")) || ("https".equals(xScheme) && xPort.equals("443"))) ? "" : ":" + xPort);
+            if (xPrefix != null) {
+                baseUrl += xPrefix;
+            }
+            return baseUrl;
+        } else {
+            // If not, use the default
+            String scheme = request.getScheme();
+            String host = request.getServerName();
+            int port = request.getServerPort();
+            String contextPath = request.getContextPath();
+            String baseUrl = scheme + "://" + host + ((("http".equals(scheme) && port == 80) || ("https".equals(scheme) && port == 443)) ? "" : ":" + port) + contextPath;
+            return baseUrl;
+        }
     }
 
     public static String getAjaxUrl(HttpServletRequest request) {
